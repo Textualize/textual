d0fd20b26b0c9740247e9e4b6dbba46eae2f275c
Filipe Laíns <lains@riseup.net>
2021-06-02 18:44:00 +0100

    examples/simple: fix constructor and run method
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- rich/tui/examples/simple.py
+++ rich/tui/examples/simple.py
@@ -1,6 +1,8 @@
+from rich.console import Console
 from rich.layout import Layout
 from rich.table import Table
 from rich.tui.app import App
+from rich.tui.view import View
 
 from rich.widgets.color_changer import ColorChanger
 
@@ -8,8 +10,13 @@ from rich.widgets.color_changer import ColorChanger
 class SimpleApp(App):
     table: Table
 
-    def __init__(self):
-        super().__init__()
+    def __init__(
+        self,
+        console: Console = None,
+        view: View = None,
+        screen: bool = True,
+    ):
+        super().__init__(console, view, screen, title='Simple App')
 
         self.table = table = Table("foo", "bar", "baz")
         table.add_row("1", "2", "3")
@@ -25,4 +32,4 @@ class SimpleApp(App):
 
 if __name__ == "__main__":
     app = SimpleApp()
-    app.run_message_loop()
+    app.run()
