78dd3c30a8aa2c4f0939091851759aff329bf309
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-09-04 13:38:46 -0500

    Add tests for contexvar propagation & callable classes

--- tests/test_message_pump.py
+++ tests/test_message_pump.py
@@ -1,4 +1,5 @@
 import asyncio
+import contextvars
 import time
 
 import pytest
@@ -7,7 +8,7 @@ from textual.message_pump import MessagePump
 
 
 @pytest.mark.asyncio
-async def test_execute_in_thread():
+async def test_execute_in_thread_parallelism():
 
     sleep_time = 0.1
 
@@ -42,3 +43,42 @@ async def test_execute_in_thread():
     elapsed = end-start
     assert elapsed < sleep_time * 2, elapsed
 
+
+@pytest.mark.asyncio
+async def test_execute_in_thread_context():
+    # Test that execute_in_thread carries any contextvars
+    # into the thread that the blocking call is executed in
+
+    ctxvar = contextvars.ContextVar("ctxvar")
+
+    def in_thread_sync() -> int:
+        return ctxvar.get()
+
+    async def in_thread_async() -> int:
+        return ctxvar.get()
+
+    pump = MessagePump()
+    ctxvar.set(1)
+
+    assert (await pump.execute_in_thread(in_thread_sync)) == 1
+    assert (await pump.execute_in_thread(in_thread_async)) == 1
+
+
+@pytest.mark.asyncio
+async def test_execute_in_thread_callable_class():
+    # Test that execute_in_thread carries any contextvars
+    # into the thread that the blocking call is executed in
+
+    class TargetSync:
+
+        def __call__(self) -> int:
+            return 1
+    
+    class TargetAsync:
+        async def __call__(self) -> int:
+            return 1
+
+    pump = MessagePump()
+
+    assert (await pump.execute_in_thread(TargetAsync())) == 1
+    assert (await pump.execute_in_thread(TargetSync())) == 1
