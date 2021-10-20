a62949bcb5312486b953c586d8b892246234a5b5
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-10-20 23:38:55 +0530

    It's a set...

--- src/textual/layouts/grid.py
+++ src/textual/layouts/grid.py
@@ -349,7 +349,7 @@ class GridLayout(Layout):
             if area and widget.visible
         )
 
-        free_slots = dict(product(range(column_count), range(row_count)))
+        free_slots = set(product(range(column_count), range(row_count)))
         order = 1
         from_corners = Region.from_corners
         gutter = Offset(self.column_gutter, self.row_gutter)
