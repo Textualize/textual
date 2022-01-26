d1738cffb589c923d537e01d392c51640b6292f0
Darren Burns <darrenb900@gmail.com>
2022-01-26 11:24:03 +0000

    Revert dev sandbox

--- examples/dev_sandbox.py
+++ examples/dev_sandbox.py
@@ -1,28 +1,23 @@
-from rich.console import RenderableType
-from rich.panel import Panel
-
 from textual.app import App
 from textual.widget import Widget
 
 
-class PanelWidget(Widget):
-    def render(self) -> RenderableType:
-        return Panel("hello world!", title="Title", height=4)
-
-
 class BasicApp(App):
-    """Sandbox application used for testing/development by Textual developers"""
+    """A basic app demonstrating CSS"""
 
     def on_load(self):
         """Bind keys here."""
         self.bind("tab", "toggle_class('#sidebar', '-active')")
         self.bind("a", "toggle_class('#header', '-visible')")
-        self.bind("c", "toggle_class('#content', '-content-visible')")
 
     def on_mount(self):
         """Build layout here."""
-        self.mount(header=PanelWidget(), content=PanelWidget(), footer=PanelWidget())
-        self.view.refresh_layout()
+        self.mount(
+            header=Widget(),
+            content=Widget(),
+            footer=Widget(),
+            sidebar=Widget(),
+        )
 
 
-BasicApp.run(log="textual.log")
+BasicApp.run(css_file="dev_sandbox.css", watch_css=True, log="textual.log")
