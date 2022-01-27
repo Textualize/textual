5d8c04b92a4f3dcb363f2fa817618f0a4ec57007
Darren Burns <darrenb900@gmail.com>
2022-01-27 13:40:28 +0000

    Tidy up HorizontalLayout, remove unused params

--- src/textual/layouts/horizontal.py
+++ src/textual/layouts/horizontal.py
@@ -4,23 +4,16 @@ from typing import Iterable
 
 from textual._loop import loop_last
 from textual.css.styles import Styles
-from textual.geometry import SpacingDimensions, Spacing, Size, Offset, Region
+from textual.geometry import Size, Offset, Region
 from textual.layout import Layout, WidgetPlacement
 from textual.view import View
 from textual.widget import Widget
 
 
 class HorizontalLayout(Layout):
-    def __init__(
-        self,
-        *,
-        z: int = 0,
-        gutter: SpacingDimensions = (0, 0, 0, 0),
-    ):
-        self.z = z
-        self.gutter = Spacing.unpack(gutter)
-        self._max_widget_width = 0
-        super().__init__()
+    """Used to layout Widgets horizontally on screen, from left to right. Since Widgets naturally
+    fill the space of their parent container, all widgets used in a horizontal layout should have a specified.
+    """
 
     def get_widgets(self, view: View) -> Iterable[Widget]:
         return view.children
@@ -28,18 +21,20 @@ class HorizontalLayout(Layout):
     def arrange(
         self, view: View, size: Size, scroll: Offset
     ) -> Iterable[WidgetPlacement]:
-        width, height = size
-        gutter = self.gutter
-        gutter_width = max(gutter.right, gutter.left)
-        x, y = self.gutter.top_left
-
+        parent_width, parent_height = size
+        x, y = 0, 0
         for last, widget in loop_last(view.children):
             styles: Styles = widget.styles
-            render_height = styles.height if styles else height
+            if styles.height:
+                render_height = int(
+                    styles.height.resolve_dimension(size, view.app.size)
+                )
+            else:
+                render_height = parent_height
             if styles.width:
                 render_width = int(styles.width.resolve_dimension(size, view.app.size))
             else:
-                render_width = width
+                render_width = parent_width
             region = Region(x, y, render_width, render_height)
-            yield WidgetPlacement(region, widget, self.z)
-            x += render_width + (gutter.right if last else gutter_width)
+            yield WidgetPlacement(region, widget, order=0)
+            x += render_width
