e2823b32d4dec5f77fee4de0e75454a9f9d59315
Will McGugan <willmcgugan@gmail.com>
2022-02-07 16:43:44 +0000

    typing fix for 3.7

--- src/textual/_box.py
+++ src/textual/_box.py
@@ -1,6 +1,11 @@
 from __future__ import annotations
 
-from typing import Literal
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
 
 from rich.console import Console, ConsoleOptions, RenderResult, RenderableType
 from rich.segment import Segment
