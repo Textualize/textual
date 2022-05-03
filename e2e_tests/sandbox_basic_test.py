078b7c151b39381586c6f430a4a30fe83be8709a
Olivier Philippon <olivier@textualize.io>
2022-05-03 13:51:33 +0100

    [e2e] The smoke tests no longer rely on the `multiprocessing` package
    Running the scripts with this package leads to the following cryptic error in our CI, when run in macOS:
    ```
    RuntimeError:
            An attempt has been made to start a new process before the
            current process has finished its bootstrapping phase.
    
            This probably means that you are not using fork to start your
            child processes and you have forgotten to use the proper idiom
            in the main module:
    
                if __name__ == '__main__':
                    freeze_support()
                    ...
    
            The "freeze_support()" line can be omitted if the program
            is not going to be frozen to produce an executable.
    ```
    (and Python's docs clearly state that this `freeze_support` function has no effect when invoked on any operating system other than Windows ¯\_(ツ)_/¯ )

--- e2e_tests/sandbox_basic_test.py
+++ e2e_tests/sandbox_basic_test.py
@@ -1,32 +1,55 @@
 from __future__ import annotations
+
+import shlex
 import sys
-import multiprocessing
+import subprocess
+import threading
 from pathlib import Path
 
-project_root = Path(__file__).parent.parent
-for python_path in [str(project_root), str(project_root / "src")]:
-    sys.path.append(python_path)
+target_script_name = "basic"
+script_time_to_live = 2.0  # in seconds
+
+if len(sys.argv) > 1:
+    target_script_name = sys.argv[1]
+if len(sys.argv) > 2:
+    script_time_to_live = float(sys.argv[2])
+
+e2e_root = Path(__file__).parent
+
+completed_process = None
 
 
-def launch_sandbox_script():
-    from sandbox.basic import app as basic_app
+def launch_sandbox_script(python_file_name: str) -> None:
+    global completed_process
 
-    basic_app.run()
+    command = f"{sys.executable} ./test_apps/{shlex.quote(python_file_name)}.py"
+    print(f"Launching command '{command}'...")
+    try:
+        completed_process = subprocess.run(
+            command, shell=True, check=True, capture_output=True, cwd=str(e2e_root)
+        )
+    except subprocess.CalledProcessError as err:
+        print(f"Process error: {err.stderr}")
+        raise
 
 
-# The following line seems to be required in order to have this work in the MacOS part
-# of our CI - despite the fact that Python docs say that this function
-# "has no effect when invoked on any operating system other than Windows" 🤔
-multiprocessing.freeze_support()
+thread = threading.Thread(
+    target=launch_sandbox_script, args=(target_script_name,), daemon=True
+)
+thread.start()
 
-process = multiprocessing.Process(target=launch_sandbox_script, daemon=True)
-process.start()
+print(
+    f"Launching Python script in a sub-thread; we'll wait for it for {script_time_to_live} seconds..."
+)
+thread.join(timeout=script_time_to_live)
+print("The wait is over.")
 
-process.join(timeout=2)
-
-process_still_running = process.exitcode is None
+process_still_running = completed_process is None
 process_was_able_to_run_without_errors = process_still_running
-if process_still_running:
-    process.kill()
+
+if process_was_able_to_run_without_errors:
+    print("Python script is still running :-)")
+else:
+    print("Python script is no longer running :-/")
 
 sys.exit(0 if process_was_able_to_run_without_errors else 1)
---
+++ e2e_tests/test_apps/basic.py
@@ -0,0 +1,150 @@
+from pathlib import Path
+
+from rich.align import Align
+from rich.console import RenderableType
+from rich.syntax import Syntax
+from rich.text import Text
+
+from textual.app import App
+from textual.widget import Widget
+from textual.widgets import Static
+
+CODE = '''
+class Offset(NamedTuple):
+    """A point defined by x and y coordinates."""
+
+    x: int = 0
+    y: int = 0
+
+    @property
+    def is_origin(self) -> bool:
+        """Check if the point is at the origin (0, 0)"""
+        return self == (0, 0)
+
+    def __bool__(self) -> bool:
+        return self != (0, 0)
+
+    def __add__(self, other: object) -> Offset:
+        if isinstance(other, tuple):
+            _x, _y = self
+            x, y = other
+            return Offset(_x + x, _y + y)
+        return NotImplemented
+
+    def __sub__(self, other: object) -> Offset:
+        if isinstance(other, tuple):
+            _x, _y = self
+            x, y = other
+            return Offset(_x - x, _y - y)
+        return NotImplemented
+
+    def __mul__(self, other: object) -> Offset:
+        if isinstance(other, (float, int)):
+            x, y = self
+            return Offset(int(x * other), int(y * other))
+        return NotImplemented
+'''
+
+
+lorem = Text.from_markup(
+    """Lorem ipsum dolor sit amet, consectetur adipiscing elit. In velit libero, volutpat nec hendrerit at, faucibus in odio. Aliquam hendrerit nibh sed quam volutpat maximus. Nullam suscipit convallis lorem quis sodales. In tristique lobortis ante et dictum. Ut at finibus ipsum. In urna dolor, placerat et mi facilisis, congue sollicitudin massa. Phasellus felis turpis, cursus eu lectus et, porttitor malesuada augue. Sed feugiat volutpat velit, sollicitudin fringilla velit bibendum faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In velit libero, volutpat nec hendrerit at, faucibus in odio. Aliquam hendrerit nibh sed quam volutpat maximus. Nullam suscipit convallis lorem quis sodales. In tristique lobortis ante et dictum. Ut at finibus ipsum. In urna dolor, placerat et mi facilisis, congue sollicitudin massa. Phasellus felis turpis, cursus eu lectus et, porttitor malesuada augue. Sed feugiat volutpat velit, sollicitudin fringilla velit bibendum faucibus. """
+    """Lorem ipsum dolor sit amet, consectetur adipiscing elit. In velit libero, volutpat nec hendrerit at, faucibus in odio. Aliquam hendrerit nibh sed quam volutpat maximus. Nullam suscipit convallis lorem quis sodales. In tristique lobortis ante et dictum. Ut at finibus ipsum. In urna dolor, placerat et mi facilisis, congue sollicitudin massa. Phasellus felis turpis, cursus eu lectus et, porttitor malesuada augue. Sed feugiat volutpat velit, sollicitudin fringilla velit bibendum faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In velit libero, volutpat nec hendrerit at, faucibus in odio. Aliquam hendrerit nibh sed quam volutpat maximus. Nullam suscipit convallis lorem quis sodales. In tristique lobortis ante et dictum. Ut at finibus ipsum. In urna dolor, placerat et mi facilisis, congue sollicitudin massa. Phasellus felis turpis, cursus eu lectus et, porttitor malesuada augue. Sed feugiat volutpat velit, sollicitudin fringilla velit bibendum faucibus. """
+)
+
+
+class TweetHeader(Widget):
+    def render(self) -> RenderableType:
+        return Text("Lorem Impsum", justify="center")
+
+
+class TweetBody(Widget):
+    def render(self) -> Text:
+        return lorem
+
+
+class Tweet(Widget):
+    pass
+
+
+class OptionItem(Widget):
+    def render(self) -> Text:
+        return Text("Option")
+
+
+class Error(Widget):
+    def render(self) -> Text:
+        return Text("This is an error message", justify="center")
+
+
+class Warning(Widget):
+    def render(self) -> Text:
+        return Text("This is a warning message", justify="center")
+
+
+class Success(Widget):
+    def render(self) -> Text:
+        return Text("This is a success message", justify="center")
+
+
+class BasicApp(App):
+    """A basic app demonstrating CSS"""
+
+    def on_load(self):
+        """Bind keys here."""
+        self.bind("tab", "toggle_class('#sidebar', '-active')")
+
+    def on_mount(self):
+        """Build layout here."""
+        self.mount(
+            header=Static(
+                Text.from_markup(
+                    "[b]This is a [u]Textual[/u] app, running in the terminal"
+                ),
+            ),
+            content=Widget(
+                Tweet(
+                    TweetBody(),
+                    # Widget(
+                    #     Widget(classes={"button"}),
+                    #     Widget(classes={"button"}),
+                    #     classes={"horizontal"},
+                    # ),
+                ),
+                Widget(
+                    Static(Syntax(CODE, "python"), classes="code"),
+                    classes="scrollable",
+                ),
+                Error(),
+                Tweet(TweetBody()),
+                Warning(),
+                Tweet(TweetBody()),
+                Success(),
+            ),
+            footer=Widget(),
+            sidebar=Widget(
+                Widget(classes="title"),
+                Widget(classes="user"),
+                OptionItem(),
+                OptionItem(),
+                OptionItem(),
+                Widget(classes="content"),
+            ),
+        )
+
+    async def on_key(self, event) -> None:
+        await self.dispatch_key(event)
+
+    def key_d(self):
+        self.dark = not self.dark
+
+    def key_x(self):
+        self.panic(self.tree)
+
+
+css_file = Path(__file__).parent / "basic.css"
+app = BasicApp(
+    css_file=str(css_file), watch_css=True, log="textual.log", log_verbosity=0
+)
+
+if __name__ == "__main__":
+    app.run()
