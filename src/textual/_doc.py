d3c800e66faad55d9a6f62d52e67137b5d29d374
Will McGugan <willmcgugan@gmail.com>
2022-05-27 10:47:36 +0100

    reduce screenshot time

--- src/textual/_doc.py
+++ src/textual/_doc.py
@@ -7,7 +7,7 @@ def format_svg(source, language, css_class, options, md, attrs, **kwargs):
     """A superfences formatter to insert a SVG screenshot."""
 
     os.environ["TEXTUAL"] = "headless"
-    os.environ["TEXTUAL_SCREENSHOT"] = "0.2"
+    os.environ["TEXTUAL_SCREENSHOT"] = "0.1"
     os.environ["COLUMNS"] = attrs.get("columns", "80")
     os.environ["LINES"] = attrs.get("lines", "24")
     path = attrs.get("path")
