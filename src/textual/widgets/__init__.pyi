cc540809d27eb341af95a6b086de9d9ad714ea3d
Will McGugan <willmcgugan@gmail.com>
2022-06-21 11:39:49 +0100

    fixed typing import

--- src/textual/widgets/__init__.pyi
+++ src/textual/widgets/__init__.pyi
@@ -1,6 +1,6 @@
 # This stub file must re-export every classes exposed in the __init__.py's `__all__` list:
 from ._button import Button as Button
-from ._datatable import DataTable
+from ._data_table import DataTable
 from ._directory_tree import DirectoryTree as DirectoryTree
 from ._footer import Footer as Footer
 from ._header import Header as Header
