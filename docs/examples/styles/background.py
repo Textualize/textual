cbd0a6ec364cecf28a5cc1121dfc70af3d179483
Will McGugan <willmcgugan@gmail.com>
2022-08-05 09:13:16 +0100

    Update docs/examples/styles/background.py
    Co-authored-by: darrenburns <darrenburns@users.noreply.github.com>

--- docs/examples/styles/background.py
+++ docs/examples/styles/background.py
@@ -21,9 +21,9 @@ class BackgroundApp(App):
     """
 
     def compose(self):
-        yield Static("Hello, World!", id="static1")
-        yield Static("Hello, World!", id="static2")
-        yield Static("Hello, World!", id="static3")
+        yield Static("Widget 1", id="static1")
+        yield Static("Widget 2", id="static2")
+        yield Static("Widget 3", id="static3")
 
 
 app = BackgroundApp()
