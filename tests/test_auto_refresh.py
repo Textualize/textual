e2c2f1abab2238b08a8c9b2245455c3b207bc840
Will McGugan <willmcgugan@gmail.com>
2022-08-18 11:10:43 +0100

    relax constraint again

--- tests/test_auto_refresh.py
+++ tests/test_auto_refresh.py
@@ -25,4 +25,4 @@ def test_auto_refresh():
     elapsed = app.run(quit_after=1, headless=True)
     assert elapsed is not None
     # CI can run slower, so we need to give this a bit of margin
-    assert elapsed >= 0.3 and elapsed < 0.5
+    assert elapsed >= 0.3 and elapsed < 0.6
