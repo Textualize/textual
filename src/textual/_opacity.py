b2f9d57eac919d0b40b646756a88060cdb785a98
Darren Burns <darrenb900@gmail.com>
2022-08-31 17:40:38 +0100

    Add a docstring for _apply_opacity

--- src/textual/_opacity.py
+++ src/textual/_opacity.py
@@ -11,6 +11,16 @@ def _apply_opacity(
     base_background: Color,
     opacity: float,
 ) -> Iterable[Segment]:
+    """Takes an iterable of foreground Segments and blends them into the supplied
+    background color, yielding copies of the Segments with blended foreground and
+    background colors applied.
+
+    Args:
+        segments (Iterable[Segment]): The segments in the foreground.
+        base_background (Color): The background color to blend foreground into.
+        opacity (float): The blending factor. A value of 1.0 means output segments will
+            have identical foreground and background colors to input segments.
+    """
     _Segment = Segment
     for segment in segments:
         text, style, _ = segment
