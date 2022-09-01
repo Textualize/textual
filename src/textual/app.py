99ffcf50406e9bedf24ea01d3a3e9fdb84aa1b02
Darren Burns <darrenb900@gmail.com>
2022-09-01 16:00:24 +0100

    Simplify

--- src/textual/app.py
+++ src/textual/app.py
@@ -226,9 +226,7 @@ class App(Generic[ReturnType], DOMNode):
 
         # We want the CSS path to be resolved from the location of the App subclass
         css_path = css_path or self.CSS_PATH
-        if css_path is None:
-            css_path = None
-        else:
+        if css_path is not None:
             if isinstance(css_path, str):
                 css_path = Path(css_path)
             css_path = _make_path_object_relative(css_path, self) if css_path else None
