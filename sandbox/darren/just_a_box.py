b6e133c1af269c0d44da7c3fc2971a915feca37d
Darren Burns <darrenb900@gmail.com>
2022-09-07 12:12:17 +0100

    Cleanup easing example

--- sandbox/darren/just_a_box.py
+++ sandbox/darren/just_a_box.py
@@ -5,10 +5,14 @@ from textual.widgets import Static
 
 
 class JustABox(App):
+
+    css_path = "../darren/just_a_box.css"
+
     def compose(self) -> ComposeResult:
         yield Static("Hello, world!", classes="box1")
 
 
+app = JustABox(watch_css=True)
+
 if __name__ == "__main__":
-    app = JustABox(css_path="../darren/just_a_box.css", watch_css=True)
     app.run()
