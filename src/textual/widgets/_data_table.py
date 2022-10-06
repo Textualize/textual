212f61ad7b6694e8b2a1d0457ea7f3518059d6a9
Will McGugan <willmcgugan@gmail.com>
2022-10-06 13:22:44 +0100

    docstring

--- src/textual/widgets/_data_table.py
+++ src/textual/widgets/_data_table.py
@@ -62,6 +62,7 @@ class Column:
 
     @property
     def render_width(self) -> int:
+        """Width in cells, required to render a column."""
         if self.auto_width:
             return self.content_width + 2
         else:
