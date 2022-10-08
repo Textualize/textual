4ff99bc72c166c05d29fc770ecd4e82adc79de99
Will McGugan <willmcgugan@gmail.com>
2022-10-08 14:55:01 +0100

    remove hack

--- src/textual/widget.py
+++ src/textual/widget.py
@@ -1734,8 +1734,6 @@ class Widget(DOMNode):
             self._content_height_cache = (None, 0)
             self._rich_style_cache.clear()
             self._repaint_required = True
-            # if isinstance(self.parent, Widget) and self.styles.auto_dimensions:
-            #     self.parent.refresh(layout=True)
 
         self.check_idle()
 
