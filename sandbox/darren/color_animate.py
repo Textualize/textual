ad2af62029763fcc9cff19e15bc3648ad5752a2a
Darren Burns <darrenb900@gmail.com>
2022-10-20 10:01:24 +0100

    Update sandbox example to test animated dark mode toggle

--- sandbox/darren/color_animate.py
+++ sandbox/darren/color_animate.py
@@ -1,4 +1,5 @@
 from textual.app import App, ComposeResult
+from textual.binding import Binding
 from textual.color import Color
 from textual.widgets import Static
 
@@ -7,11 +8,16 @@ END_COLOR = Color.parse("#0000FF0F")
 
 
 class ColorAnimate(App):
+    BINDINGS = [Binding("d", action="toggle_dark", description="Dark mode")]
+
     def compose(self) -> ComposeResult:
-        box = Static("Hello, world", id="box")
-        box.styles.background = START_COLOR
-        self.box = box
-        yield box
+        self.box = Static("Hello, world", id="box")
+        self.box.styles.background = START_COLOR
+
+        self.another_box = Static("Another box with $boost", id="another-box")
+
+        yield self.box
+        yield self.another_box
 
     def key_a(self):
         self.animator.animate(self.box.styles, "background", END_COLOR, duration=2.0)
