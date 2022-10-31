4e06df191fcbf0b8e6e34c9a6bc65f906be49339
Felix Ingram <f.ingram@gmail.com>
2022-10-31 15:23:27 +0000

    Add docstring

--- src/textual/debounce.py
+++ src/textual/debounce.py
@@ -7,6 +7,17 @@ from .timer import Timer as TimerClass
 
 
 def debounce(timeout: float) -> Callable:
+    """Debounce repeated events.
+
+    If event handlers perform expensive operations, then it is sometimes desirable to
+    limit the number of calls to the handler. The debounce decorartor will (re)start
+    a timer for every event received and only call the handler if no events have occured
+    in the time period provided.
+
+    Args:
+        timeout (float): the number of seconds to wait for repeated events.
+    """
+
     def decorator(f: Callable) -> Callable:
         timer: Optional[TimerClass] = None
 
@@ -14,10 +25,11 @@ def debounce(timeout: float) -> Callable:
         async def wrapper(self: App, *args, **kwargs) -> None:
             nonlocal timer
             if timer:
-                timer.stop_no_wait()
-            timer = self.set_timer(
-                timeout, lambda: asyncio.create_task(f(self, *args, *kwargs))
-            )
+                timer.reset()
+            else:
+                timer = self.set_timer(
+                    timeout, lambda: asyncio.create_task(f(self, *args, *kwargs))
+                )
 
         return wrapper
 
