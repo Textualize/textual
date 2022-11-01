4d8057737582d4174621497b65e69e6fc4169c4c
Darren Burns <darrenb900@gmail.com>
2022-11-01 16:54:10 +0000

    Remove unused import

--- src/textual/_typing.py
+++ src/textual/_typing.py
@@ -6,6 +6,6 @@ else:  # pragma: no cover
     from typing_extensions import TypeAlias
 
 if sys.version_info >= (3, 8):
-    from typing import Final, Iterable, Literal, Protocol, TypedDict
+    from typing import Final, Literal, Protocol, TypedDict
 else:
     from typing_extensions import Final, Literal, Protocol, TypedDict
