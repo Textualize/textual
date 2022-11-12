2605785288d5968033b0191f4f0c63fd4528357a
Will McGugan <willmcgugan@gmail.com>
2022-11-12 12:12:23 +0000

    use per_counter

--- tests/test_animation.py
+++ tests/test_animation.py
@@ -1,5 +1,4 @@
-import asyncio
-from time import time
+from time import perf_counter
 
 from textual.app import App, ComposeResult
 from textual.widgets import Static
@@ -28,18 +27,18 @@ async def test_animate_height() -> None:
         static = app.query_one(Static)
         assert static.size.height == 1
         static.styles.animate("height", 100, duration=0.5, easing="linear")
-        start = time()
+        start = perf_counter()
 
         # Wait for half the animation
         await pilot.pause(0.25)
         # Check we reached the half way point
         assert static.styles.height.value >= 50
-        elapsed = time() - start
+        elapsed = perf_counter() - start
         # Check at least that much time has elapsed
         assert 0.5 > elapsed > 0.25
         # Wait for the animation to finished
         await pilot.wait_for_animation()
-        elapsed = time() - start
+        elapsed = perf_counter() - start
         # Check that the full time has elapsed
         assert elapsed > 0.5
         # Check the height reached the maximum
