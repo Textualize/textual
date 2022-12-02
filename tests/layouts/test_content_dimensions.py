1def555a76500cdd46774ded95d07af0fa61b004
Rodrigo Girão Serrão <5621605+RodrigoGiraoSerrao@users.noreply.github.com>
2022-12-02 15:43:17 +0000

    Add tests for dimensions of empty containers.

---
+++ tests/layouts/test_content_dimensions.py
@@ -0,0 +1,28 @@
+import pytest
+
+from textual.layouts.grid import GridLayout
+from textual.layouts.horizontal import HorizontalLayout
+from textual.layouts.vertical import VerticalLayout
+from textual.geometry import Size
+from textual.widget import Widget
+
+
+LAYOUTS = [GridLayout, HorizontalLayout, VerticalLayout]
+
+
+@pytest.mark.parametrize("layout", LAYOUTS)
+def test_empty_widget_height(layout):
+    """Test that an empty widget has height equal to 0."""
+    l = layout()
+    # Make sure this measurement does not depend on the width.
+    assert l.get_content_height(Widget(), Size(80, 24), Size(80, 24), 24) == 0
+    assert l.get_content_height(Widget(), Size(80, 24), Size(80, 24), 20) == 0
+    assert l.get_content_height(Widget(), Size(80, 24), Size(80, 24), 10) == 0
+    assert l.get_content_height(Widget(), Size(80, 24), Size(80, 24), 0) == 0
+
+
+@pytest.mark.parametrize("layout", LAYOUTS)
+def test_empty_widget_width(layout):
+    """Test that an empty widget has width equal to 0."""
+    l = layout()
+    assert l.get_content_width(Widget(), Size(80, 24), Size(80, 24)) == 0
