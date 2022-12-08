e94f6359f8d6c15d0445b98d2df7f98ebd204c97
Rodrigo Girão Serrão <5621605+RodrigoGiraoSerrao@users.noreply.github.com>
2022-12-08 08:32:31 +0000

    Deployed b9cef559 with MkDocs version: 1.4.2

---
+++ blog/snippets/2022-12-07-responsive-app-background-task/blocking02.py
@@ -0,0 +1,44 @@
+import asyncio
+from random import randint
+import time
+
+from textual.app import App, ComposeResult
+from textual.color import Color
+from textual.containers import Grid, Vertical
+from textual.widget import Widget
+from textual.widgets import Footer, Label
+
+
+class ColourChanger(Widget):
+    def on_click(self) -> None:
+        self.styles.background = Color(
+            randint(1, 255),
+            randint(1, 255),
+            randint(1, 255),
+        )
+
+
+class MyApp(App[None]):
+    BINDINGS = [("l", "load", "Load data")]
+    CSS = """
+    Grid {
+        grid-size: 2;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Grid(
+            ColourChanger(),
+            Vertical(id="log"),
+        )
+        yield Footer()
+
+    def action_load(self) -> None:  # (1)!
+        asyncio.create_task(self._do_long_operation())  # (2)!
+
+    async def _do_long_operation(self) -> None:  # (3)!
+        time.sleep(5)
+        self.query_one("#log").mount(Label("Data loaded ✅"))
+
+
+MyApp().run()
