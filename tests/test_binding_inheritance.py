Textualize/textual

commit b48a1402b8103ca16d5e338538620e9e08fb2c0e

add inherited bindings

src/textual/_compositor.py
@@ -458,7 +458,6 @@ class Compositor:
 
         # Add top level (root) widget
         add_widget(root, size.region, size.region, ((0,),), layer_order, size.region)
-        root.log(map)
         return map, widgets
 
     @property

src/textual/dom.py
@@ -22,7 +22,7 @@ from rich.tree import Tree
 
 from ._context import NoActiveAppError
 from ._node_list import NodeList
-from .binding import Bindings, BindingType
+from .binding import Binding, Bindings, BindingType
 from .color import BLACK, WHITE, Color
 from .css._error_tools import friendly_list
 from .css.constants import VALID_DISPLAY, VALID_VISIBILITY
@@ -97,8 +97,15 @@ class DOMNode(MessagePump):
 
     # True if this node inherits the CSS from the base class.
     _inherit_css: ClassVar[bool] = True
+
+    # True to inherit bindings from base class
+    _inherit_bindings: ClassVar[bool] = True
+
     # List of names of base classes that inherit CSS
     _css_type_names: ClassVar[frozenset[str]] = frozenset()
+
+    # Generated list of bindings
+    _merged_bindings: ClassVar[Bindings] | None = None
 
     def __init__(
         self,
@@ -127,7 +134,7 @@ class DOMNode(MessagePump):
         self._auto_refresh: float | None = None
         self._auto_refresh_timer: Timer | None = None
         self._css_types = {cls.__name__ for cls in self._css_bases(self.__class__)}
-        self._bindings = Bindings(self.BINDINGS)
+        self._bindings = self._merged_bindings or Bindings()
         self._has_hover_style: bool = False
         self._has_focus_within: bool = False
 
@@ -152,12 +159,16 @@ class DOMNode(MessagePump):
         """Perform an automatic refresh (set with auto_refresh property)."""
         self.refresh()
 
-    def __init_subclass__(cls, inherit_css: bool = True) -> None:
+    def __init_subclass__(
+        cls, inherit_css: bool = True, inherit_bindings: bool = True
+    ) -> None:
         super().__init_subclass__()
         cls._inherit_css = inherit_css
+        cls._inherit_bindings = inherit_bindings
         css_type_names: set[str] = set()
         for base in cls._css_bases(cls):
             css_type_names.add(base.__name__)
+        cls._merged_bindings = cls._merge_bindings()
         cls._css_type_names = frozenset(css_type_names)
 
     def get_component_styles(self, name: str) -> RenderStyles:
@@ -204,6 +215,25 @@ class DOMNode(MessagePump):
                     break
             else:
                 break
+
+    @classmethod
+    def _merge_bindings(cls) -> Bindings:
+        """Merge bindings from base classes.
+
+        Returns:
+            Bindings: Merged bindings.
+        """
+        bindings: list[Bindings] = []
+
+        for base in reversed(cls.__mro__):
+            if issubclass(base, DOMNode):
+                if not base._inherit_bindings:
+                    bindings.clear()
+                bindings.append(Bindings(base.BINDINGS))
+        keys = {}
+        for bindings_ in bindings:
+            keys.update(bindings_.keys)
+        return Bindings(keys.values())
 
     def _post_register(self, app: App) -> None:
         """Called when the widget is registered

src/textual/widget.py
@@ -174,6 +174,12 @@ class Widget(DOMNode):
     BINDINGS = [
         Binding("up", "scroll_up", "Scroll Up", show=False),
         Binding("down", "scroll_down", "Scroll Down", show=False),
+        Binding("left", "scroll_left", "Scroll Up", show=False),
+        Binding("right", "scroll_right", "Scroll Right", show=False),
+        Binding("home", "scroll_home", "Scroll Home", show=False),
+        Binding("end", "scroll_end", "Scroll End", show=False),
+        Binding("pageup", "page_up", "Page Up", show=False),
+        Binding("pagedown", "page_down", "Page Down", show=False),
     ]
 
     DEFAULT_CSS = """
@@ -1816,9 +1822,13 @@ class Widget(DOMNode):
         can_focus: bool | None = None,
         can_focus_children: bool | None = None,
         inherit_css: bool = True,
+        inherit_bindings: bool = True,
     ) -> None:
         base = cls.__mro__[0]
-        super().__init_subclass__(inherit_css=inherit_css)
+        super().__init_subclass__(
+            inherit_css=inherit_css,
+            inherit_bindings=inherit_bindings,
+        )
         if issubclass(base, Widget):
             cls.can_focus = base.can_focus if can_focus is None else can_focus
             cls.can_focus_children = (
@@ -2345,53 +2355,21 @@ class Widget(DOMNode):
     def _on_scroll_to_region(self, message: messages.ScrollToRegion) -> None:
         self.scroll_to_region(message.region, animate=True)
 
-    def _key_home(self) -> bool:
+    def action_scroll_home(self) -> None:
         if self._allow_scroll:
             self.scroll_home()
-            return True
-        return False
 
-    def _key_end(self) -> bool:
+    def action_scroll_end(self) -> None:
         if self._allow_scroll:
             self.scroll_end()
-            return True
-        return False
 
-    def _key_left(self) -> bool:
+    def action_scroll_left(self) -> None:
         if self.allow_horizontal_scroll:
             self.scroll_left()
-            return True
-        return False
 
-    def _key_right(self) -> bool:
+    def action_scroll_right(self) -> None:
         if self.allow_horizontal_scroll:
             self.scroll_right()
-            return True
-        return False
-
-    # def _key_down(self) -> bool:
-    #     if self.allow_vertical_scroll:
-    #         self.scroll_down()
-    #         return True
-    #     return False
-
-    # def _key_up(self) -> bool:
-    #     if self.allow_vertical_scroll:
-    #         self.scroll_up()
-    #         return True
-    #     return False
-
-    def _key_pagedown(self) -> bool:
-        if self.allow_vertical_scroll:
-            self.scroll_page_down()
-            return True
-        return False
-
-    def _key_pageup(self) -> bool:
-        if self.allow_vertical_scroll:
-            self.scroll_page_up()
-            return True
-        return False
 
     def action_scroll_up(self) -> None:
         if self.allow_vertical_scroll:
@@ -2400,3 +2378,11 @@ class Widget(DOMNode):
     def action_scroll_down(self) -> None:
         if self.allow_vertical_scroll:
             self.scroll_down()
+
+    def action_page_down(self) -> None:
+        if self.allow_vertical_scroll:
+            self.scroll_page_down()
+
+    def action_page_up(self) -> None:
+        if self.allow_vertical_scroll:
+            self.scroll_page_up()

src/textual/widgets/_tree.py
@@ -26,7 +26,7 @@ NodeID = NewType("NodeID", int)
 TreeDataType = TypeVar("TreeDataType")
 EventTreeDataType = TypeVar("EventTreeDataType")
 
-LineCacheKey: TypeAlias = tuple[int | tuple[int, ...], ...]
+LineCacheKey: TypeAlias = tuple[int | tuple, ...]
 
 TOGGLE_STYLE = Style.from_meta({"toggle": True})
 
@@ -199,9 +199,9 @@ class TreeNode(Generic[TreeDataType]):
 class Tree(Generic[TreeDataType], ScrollView, can_focus=True):
 
     BINDINGS = [
+        Binding("enter", "select_cursor", "Select", show=False),
         Binding("up", "cursor_up", "Cursor Up", show=False),
         Binding("down", "cursor_down", "Cursor Down", show=False),
-        Binding("enter", "select_cursor", "Select", show=False),
     ]
 
     DEFAULT_CSS = """
@@ -333,6 +333,10 @@ class Tree(Generic[TreeDataType], ScrollView, can_focus=True):
     def cursor_node(self) -> TreeNode[TreeDataType] | None:
         """TreeNode | Node: The currently selected node, or ``None`` if no selection."""
         return self._cursor_node
+
+    @property
+    def last_line(self) -> int:
+        return len(self._tree_lines) - 1
 
     def process_label(self, label: TextType):
         """Process a str or Text in to a label. Maybe overridden in a subclass to change modify how labels are rendered.
@@ -797,13 +801,35 @@ class Tree(Generic[TreeDataType], ScrollView, can_focus=True):
 
     def action_cursor_up(self) -> None:
         if self.cursor_line == -1:
-            self.cursor_line = len(self._tree_lines) - 1
+            self.cursor_line = self.last_line
         else:
             self.cursor_line -= 1
         self.scroll_to_line(self.cursor_line)
 
     def action_cursor_down(self) -> None:
+        if self.cursor_line == -1:
+            self.cursor_line = 0
         self.cursor_line += 1
+        self.scroll_to_line(self.cursor_line)
+
+    def action_page_down(self) -> None:
+        if self.cursor_line == -1:
+            self.cursor_line = 0
+        self.cursor_line += self.scrollable_content_region.height - 1
+        self.scroll_to_line(self.cursor_line)
+
+    def action_page_up(self) -> None:
+        if self.cursor_line == -1:
+            self.cursor_line = self.last_line
+        self.cursor_line -= self.scrollable_content_region.height - 1
+        self.scroll_to_line(self.cursor_line)
+
+    def action_scroll_home(self) -> None:
+        self.cursor_line = 0
+        self.scroll_to_line(self.cursor_line)
+
+    def action_scroll_end(self) -> None:
+        self.cursor_line = self.last_line
         self.scroll_to_line(self.cursor_line)
 
     def action_select_cursor(self) -> None:

commit e75f784b2c788f95e398821266fcaab0f79aa12f

Add a test for a screen binding movement, wrapping a focusable widget
This is the heart of the issue introduced by
https://github.com/Textualize/textual/pull/1170/commits/b48a1402b8103ca16d5e338538620e9e08fb2c0e
and which is being investigated in
https://github.com/Textualize/textual/issues/1343 -- the child widget can be
focused, but (as far as the author of the code is concerned) it has no
bindings. Bindings for movement-oriented keys exist on the screen which
composes up the widget into it. Up until 0.5.0 this worked just fine. As of
0.6.0, because binding inheritance was introduced, the bindings for movement
that live at the `Widget` level cause the widget that has no bindings to
appear to have bindings.

While this can potentially be worked around with the use of
inherit_bindings, this isn't a very satisfying solution and also breaks the
rule of least astonishment.

This test is going to be key to all of this. This is the test that should be
made to work without breaking any of the other currently-passing tests.

tests/test_binding_inheritance.py
@@ -164,3 +164,42 @@ async def test_focused_child_widget_with_movement_bindings() -> None:
     async with AppWithWidgetWithBindings().run_test() as pilot:
         await pilot.press("x", *MOVEMENT_KEYS, "x")
         assert pilot.app.pressed_keys == ["x", *[f"locally_{key}" for key in MOVEMENT_KEYS], "x"]
+
+##############################################################################
+class FocusableWidgetWithNoBindings(Static, can_focus=True):
+    """A widget that can receive focus but has no bindings."""
+
+class ScreenWithMovementBindings(Screen):
+    """A screen that binds keys, including movement keys."""
+
+    BINDINGS = [
+        Binding("x", "record('x')", "x"),
+        *[Binding(key, f"screen_record('{key}')", key) for key in MOVEMENT_KEYS]
+    ]
+
+    async def action_screen_record(self, key: str) -> None:
+        # Sneaky forward reference. Just for the purposes of testing.
+        await self.app.action_record(f"screen_{key}")
+
+    def compose(self) -> ComposeResult:
+        yield FocusableWidgetWithNoBindings()
+
+    def on_mount(self) -> None:
+        self.query_one(FocusableWidgetWithNoBindings).focus()
+
+class AppWithScreenWithBindingsWidgetNoBindings(AppKeyRecorder):
+    """An app with a non-default screen that handles movement key bindings."""
+
+    SCREENS = {"main":ScreenWithMovementBindings}
+
+    def on_mount(self) -> None:
+        self.push_screen("main")
+
+@pytest.mark.xfail(
+    reason="Movement keys never make it to the screen with such bindings due to key inheritance and pre-bound movement keys [issue#1343]"
+)
+async def test_focused_child_widget_with_movement_bindings_on_screen() -> None:
+    """A focused child widget, with movement bindings in the screen, should trigger screen actions."""
+    async with AppWithScreenWithBindingsWidgetNoBindings().run_test() as pilot:
+        await pilot.press("x", *MOVEMENT_KEYS, "x")
+        assert pilot.app.pressed_keys == ["x", *[f"locally_{key}" for key in MOVEMENT_KEYS], "x"]