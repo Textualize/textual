159f94a9b4773b153239a8379cdd97e4511fdf19
Dave Pearson <davep@davep.org>
2022-12-21 11:23:01 +0000

    Reformat to keep black happy

--- tests/test_dark_toggle.py
+++ tests/test_dark_toggle.py
@@ -1,15 +1,18 @@
 from textual.app import App
 
+
 class OnLoadDarkSwitch(App[None]):
     """App for testing toggling dark mode in on_load."""
 
     def on_load(self) -> None:
         self.dark = not self.dark
 
+
 async def test_toggle_dark_on_load() -> None:
     """It should be possible to toggle dark mode in on_load."""
     async with OnLoadDarkSwitch().run_test() as pilot:
         assert not pilot.app.dark
+
 
 class OnMountDarkSwitch(App[None]):
     """App for testing toggling dark mode in on_mount."""
@@ -17,10 +20,12 @@ class OnMountDarkSwitch(App[None]):
     def on_mount(self) -> None:
         self.dark = not self.dark
 
+
 async def test_toggle_dark_on_mount() -> None:
     """It should be possible to toggle dark mode in on_mount."""
     async with OnMountDarkSwitch().run_test() as pilot:
         assert not pilot.app.dark
+
 
 class ActionDarkSwitch(App[None]):
     """App for testing toggling dark mode from an action."""
@@ -29,6 +34,7 @@ class ActionDarkSwitch(App[None]):
 
     def action_toggle(self) -> None:
         self.dark = not self.dark
+
 
 async def test_toggle_dark_in_action() -> None:
     """It should be possible to toggle dark mode with an action."""
