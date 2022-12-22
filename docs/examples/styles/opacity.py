5457069bb9a7fc1fc9a7cdf73a984f055c38b9c7
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2022-12-22 18:38:14 +0000

    Update opacity example.

--- docs/examples/styles/opacity.py
+++ docs/examples/styles/opacity.py
@@ -1,14 +1,14 @@
 from textual.app import App
-from textual.widgets import Static
+from textual.widgets import Label
 
 
 class OpacityApp(App):
     def compose(self):
-        yield Static("opacity: 0%", id="zero-opacity")
-        yield Static("opacity: 25%", id="quarter-opacity")
-        yield Static("opacity: 50%", id="half-opacity")
-        yield Static("opacity: 75%", id="three-quarter-opacity")
-        yield Static("opacity: 100%", id="full-opacity")
+        yield Label("opacity: 0%", id="zero-opacity")
+        yield Label("opacity: 25%", id="quarter-opacity")
+        yield Label("opacity: 50%", id="half-opacity")
+        yield Label("opacity: 75%", id="three-quarter-opacity")
+        yield Label("opacity: 100%", id="full-opacity")
 
 
 app = OpacityApp(css_path="opacity.css")
