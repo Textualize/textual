85582a409351b90efbf32ea04941f86866545fec
Will McGugan <willmcgugan@gmail.com>
2022-12-29 17:56:18 +0000

    timer fix for Windoze

--- src/textual/_time.py
+++ src/textual/_time.py
@@ -14,10 +14,18 @@ else:
 
 
 if WINDOWS:
-    async def sleep(sleep_for:float) -> None:
+
+    async def sleep(sleep_for: float) -> None:
+        """An asyncio sleep.
+
+        On Windows this achieves a better granularity that asyncio.sleep(0)
+
+        Args:
+            sleep_for (float): Seconds to sleep for.
+        """
         start = time()
-        while time() - start < sleep_for - 1/1000:
+        while time() - start < sleep_for - 1 / 1000:
             await asleep(0)
-        
+
 else:
     sleep = asleep
--- src/textual/timer.py
+++ src/textual/timer.py
@@ -9,22 +9,16 @@ from __future__ import annotations
 
 import asyncio
 import weakref
-from asyncio import (
-    CancelledError,
-    Event,
-    Task,
-)
+from asyncio import CancelledError, Event, Task
 from typing import Awaitable, Callable, Union
-
 
 from rich.repr import Result, rich_repr
 
-from . import events
+from . import _clock, events
 from ._callback import invoke
 from ._context import active_app
-from . import _clock
-from ._types import MessageTarget
 from ._time import sleep
+from ._types import MessageTarget
 
 TimerCallback = Union[Callable[[], Awaitable[None]], Callable[[], None]]
 
@@ -142,7 +136,7 @@ class Timer:
         _interval = self._interval
         await self._active.wait()
         start = _clock.get_time_no_wait()
-        sleep_event = Event()
+
         while _repeat is None or count <= _repeat:
             next_timer = start + ((count + 1) * _interval)
             now = await _clock.get_time()
@@ -151,9 +145,8 @@ class Timer:
                 continue
             now = await _clock.get_time()
             wait_time = max(0, next_timer - now)
-            if wait_time > 1/1000:
-               await sleep(wait_time)
-               
+            if wait_time > 1 / 1000:
+                await sleep(wait_time)
 
             count += 1
             await self._active.wait()
