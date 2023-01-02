7fd1baf83d4c24a44457483b948889a50d312bc5
Will McGugan <willmcgugan@gmail.com>
2023-01-02 13:49:06 +0000

    comments

--- src/textual/_time.py
+++ src/textual/_time.py
@@ -1,7 +1,6 @@
 import platform
-import sys
-
-from asyncio import sleep as asyncio_sleep, get_running_loop
+from asyncio import get_running_loop
+from asyncio import sleep as asyncio_sleep
 from time import monotonic, perf_counter
 
 PLATFORM = platform.system()
@@ -15,15 +14,31 @@ else:
 
 
 if WINDOWS:
+    # sleep on windows as a resolution of 15ms
+    # Python3.11 is somewhat better, but this home-grown version beats it
+    # Deduced from practical experiments
 
     from ._win_sleep import sleep as win_sleep
 
     async def sleep(sleep_for: float) -> None:
+        """Sleep for a given number of seconds.
+
+        Args:
+            sleep_for (float): Number of seconds to sleep for.
+        """
         await get_running_loop().run_in_executor(None, win_sleep, sleep_for)
 
 else:
 
     async def sleep(sleep_for: float) -> None:
+        """Sleep for a given number of seconds.
+
+        Args:
+            sleep_for (float): Number of seconds to sleep for.
+        """
+        # From practical experiments, asyncio.sleep sleeps for at least half a millisecond too much
+        # Presumably there is overhead asyncio itself which accounts for this
+        # We will reduce the sleep to compensate, and also don't sleep at all for less than half a millisecond
         sleep_for -= 0.0005
         if sleep_for > 0:
             await asyncio_sleep(sleep_for)
