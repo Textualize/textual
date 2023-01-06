c7c352a38a3fa95c74bd6a0d002703cb013c8810
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-01-06 10:49:25 +0000

    Replace statics with labels.
    [skip ci]

--- docs/examples/styles/text_opacity.py
+++ docs/examples/styles/text_opacity.py
@@ -1,14 +1,14 @@
 from textual.app import App
-from textual.widgets import Static
+from textual.widgets import Label
 
 
 class TextOpacityApp(App):
     def compose(self):
-        yield Static("text-opacity: 0%", id="zero-opacity")
-        yield Static("text-opacity: 25%", id="quarter-opacity")
-        yield Static("text-opacity: 50%", id="half-opacity")
-        yield Static("text-opacity: 75%", id="three-quarter-opacity")
-        yield Static("text-opacity: 100%", id="full-opacity")
+        yield Label("text-opacity: 0%", id="zero-opacity")
+        yield Label("text-opacity: 25%", id="quarter-opacity")
+        yield Label("text-opacity: 50%", id="half-opacity")
+        yield Label("text-opacity: 75%", id="three-quarter-opacity")
+        yield Label("text-opacity: 100%", id="full-opacity")
 
 
 app = TextOpacityApp(css_path="text_opacity.css")
