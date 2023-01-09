a1b9d4b2c53ff35ed5a8b3071c315b08906c7f40
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-01-09 16:11:45 +0000

    Fix examples.

--- docs/examples/styles/tint.py
+++ docs/examples/styles/tint.py
@@ -8,7 +8,7 @@ class TintApp(App):
         color = Color.parse("green")
         for tint_alpha in range(0, 101, 10):
             widget = Label(f"tint: green {tint_alpha}%;")
-            widget.styles.tint = color.with_alpha(tint_alpha / 100)
+            widget.styles.tint = color.with_alpha(tint_alpha / 100)  # (1)!
             yield widget
 
 
