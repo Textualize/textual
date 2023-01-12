dc318b8c93965c23496e7a38beab758fa38dd144
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-01-12 14:39:05 +0000

    Update tests.

--- tests/test_style_properties.py
+++ tests/test_style_properties.py
@@ -1,4 +1,8 @@
+import pytest
+from rich.style import Style
+
 from textual.color import Color
+from textual.css.errors import StyleValueError
 from textual.css.styles import Styles
 
 
@@ -7,3 +11,12 @@ def test_box_normalization():
     styles = Styles()
     styles.border_left = ("none", "red")
     assert styles.border_left == ("", Color.parse("red"))
+
+
+@pytest.mark.parametrize("style_attr", ["text_style", "link_style"])
+def test_text_style_none_with_others(style_attr):
+    """Style "none" mixed with others should give custom Textual exception."""
+    styles = Styles()
+
+    with pytest.raises(StyleValueError) as exc_info:
+        setattr(styles, style_attr, "bold none underline italic")
