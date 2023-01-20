c74b81a8a68b29d8964f6f1368d5024d80ada09f
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-01-20 14:23:41 +0000

    Potential fix for #1616.

--- src/textual/widget.py
+++ src/textual/widget.py
@@ -1959,13 +1959,23 @@ class Widget(DOMNode):
         """
         show_vertical_scrollbar, show_horizontal_scrollbar = self.scrollbars_enabled
 
-        scrollbar_size_horizontal = self.styles.scrollbar_size_horizontal
-        scrollbar_size_vertical = self.styles.scrollbar_size_vertical
+        styles = self.styles
+        scrollbar_size_horizontal = styles.scrollbar_size_horizontal
+        scrollbar_size_vertical = styles.scrollbar_size_vertical
 
-        if self.styles.scrollbar_gutter == "stable":
+        if styles.scrollbar_gutter == "stable":
             # Let's _always_ reserve some space, whether the scrollbar is actually displayed or not:
             show_vertical_scrollbar = True
-            scrollbar_size_vertical = self.styles.scrollbar_size_vertical
+            scrollbar_size_vertical = styles.scrollbar_size_vertical
+
+        overflow_x = styles.overflow_x
+        show_horizontal_scrollbar = (
+            show_horizontal_scrollbar or overflow_x == "scroll"
+        ) and overflow_x != "hidden"
+        overflow_y = styles.overflow_y
+        show_vertical_scrollbar = (
+            show_vertical_scrollbar or overflow_y == "scroll"
+        ) and overflow_y != "hidden"
 
         if show_horizontal_scrollbar and show_vertical_scrollbar:
             (region, _, _, _) = region.split(
