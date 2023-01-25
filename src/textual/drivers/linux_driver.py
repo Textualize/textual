a292086a622f7cebc2d50ded9c550aa61e75a655
Will McGugan <willmcgugan@gmail.com>
2023-01-25 13:39:24 +0100

    comment and docstring

--- src/textual/drivers/linux_driver.py
+++ src/textual/drivers/linux_driver.py
@@ -145,7 +145,11 @@ class LinuxDriver(Driver):
         self._request_terminal_sync_mode_support()
         self._enable_bracketed_paste()
 
-    def _request_terminal_sync_mode_support(self):
+    def _request_terminal_sync_mode_support(self) -> None:
+        """Writes an escape sequence to query the terminal support for the sync protocol."""
+        # Terminals should ignore this sequence if not supported.
+        # Apple terminal doesn't, and writes a single 'p' in to the terminal,
+        # so we will make a special case for Apple terminal (which doesn't support sync anyway).
         if self.console._environ.get("TERM_PROGRAM", "") != "Apple_Terminal":
             self.console.file.write("\033[?2026$p")
             self.console.file.flush()
