44d48e244ef840bf327f4b0d8876e1ff6fcdd59c
Dave Pearson <davep@davep.org>
2023-01-25 14:43:41 +0000

    Fix typo in replace
    AKA: don't mix octal and hex Dave!

--- src/textual/_xterm_parser.py
+++ src/textual/_xterm_parser.py
@@ -120,7 +120,7 @@ class XTermParser(Parser[events.Event]):
                 pasted_text = "".join(paste_buffer[:-1])
                 # Note the removal of NUL characters: https://github.com/Textualize/textual/issues/1661
                 on_token(
-                    events.Paste(self.sender, text=pasted_text.replace("\x000", ""))
+                    events.Paste(self.sender, text=pasted_text.replace("\x00", ""))
                 )
                 paste_buffer.clear()
 
