ae73c4783f4df82bebe261b2d863ff5d03a6a1e1
Will McGugan <willmcgugan@gmail.com>
2023-01-27 09:58:07 +0100

    use monotonic rather than sleep

--- src/textual/_wait.py
+++ src/textual/_wait.py
@@ -1,5 +1,5 @@
 from asyncio import sleep
-from time import process_time, time
+from time import process_time, monotonic
 
 SLEEP_GRANULARITY: float = 1 / 50
 SLEEP_IDLE: float = SLEEP_GRANULARITY / 2.0
@@ -14,13 +14,13 @@ async def wait_for_idle(
         min_sleep: Minimum time to wait. Defaults to 1/50.
         max_sleep: Maximum time to wait. Defaults to 1.
     """
-    start_time = time()
+    start_time = monotonic()
 
     while True:
         cpu_time = process_time()
         await sleep(SLEEP_GRANULARITY)
         cpu_elapsed = process_time() - cpu_time
-        elapsed_time = time() - start_time
+        elapsed_time = monotonic() - start_time
         if elapsed_time >= max_sleep:
             break
         if elapsed_time > min_sleep and cpu_elapsed < SLEEP_IDLE:
