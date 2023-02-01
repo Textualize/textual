8537c78967da822faa846ca42ee2eb7fd4755efb
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-02-01 16:08:20 +0000

    Deployed 79952719 with MkDocs version: 1.4.2

---
+++ examples/app/question_title02.py
@@ -0,0 +1,27 @@
+from textual.app import App, ComposeResult
+from textual.events import Key
+from textual.widgets import Button, Header, Label
+
+
+class MyApp(App[str]):
+    TITLE = "A Question App"
+    SUB_TITLE = "The most important question"
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Label("Do you love Textual?")
+        yield Button("Yes", id="yes", variant="primary")
+        yield Button("No", id="no", variant="error")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        self.exit(event.button.id)
+
+    def on_key(self, event: Key):
+        self.title = event.key
+        self.sub_title = f"You just pressed {event.key}!"
+
+
+if __name__ == "__main__":
+    app = MyApp()
+    reply = app.run()
+    print(reply)
