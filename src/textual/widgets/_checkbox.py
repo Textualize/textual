5e3ed65395dc7ca0ade9771d68e16e740bc66da5
Dave Pearson <davep@davep.org>
2023-02-16 15:30:26 +0000

    Add a Checkbox-specific Changed message

--- src/textual/widgets/_checkbox.py
+++ src/textual/widgets/_checkbox.py
@@ -5,3 +5,8 @@ from ._toggle import ToggleButton
 
 class Checkbox(ToggleButton):
     """A check box widget that represents a boolean value."""
+
+    class Changed(ToggleButton.Changed):
+        """Posted when the value of the checkbox changes."""
+
+        namespace = "checkbox"
