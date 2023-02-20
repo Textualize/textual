122457a08a83ab39cba277a586668c4b772afd4d
Dave Pearson <davep@davep.org>
2023-02-20 11:12:40 +0000

    Swap the radio button to using a circular character for the inner

--- src/textual/widgets/_radio_button.py
+++ src/textual/widgets/_radio_button.py
@@ -21,6 +21,7 @@ class RadioButton(ToggleButton):
         self,
         label: str,
         value: bool = False,
+        button_first: bool = True,
         *,
         name: str | None = None,
         id: str | None = None,
@@ -29,13 +30,14 @@ class RadioButton(ToggleButton):
         """Initialise the radio button.
 
         Args:
+            label: The label for the toggle.
             value: The initial value of the radio button. Defaults to `False`.
+            button_first: Should the button come before the label, or after?
             name: The name of the radio button.
             id: The ID of the radio button in the DOM.
             classes: The CSS classes of the radio button.
         """
-        super().__init__(label, value, name=name, id=id, classes=classes)
+        super().__init__(label, value, button_first, name=name, id=id, classes=classes)
         self.button_prefix = "("
         self.button_suffix = ")"
-        self.button_on = "*"
-        self.button_off = " "
+        self.button_on = "⦿"
