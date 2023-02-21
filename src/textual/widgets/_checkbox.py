ec5d5666edf74527e79cee4e1ecbf68e9de39144
Dave Pearson <davep@davep.org>
2023-02-21 21:33:59 +0000

    Drop support for a Selected message on the toggle boxes
    Requested by @willmcgugan.

--- src/textual/widgets/_checkbox.py
+++ src/textual/widgets/_checkbox.py
@@ -8,12 +8,6 @@ from ._toggle import ToggleButton
 class Checkbox(ToggleButton):
     """A check box widget that represents a boolean value."""
 
-    class Selected(ToggleButton.Selected):
-        """Posted when the user selects the button."""
-
-        # https://github.com/Textualize/textual/issues/1814
-        namespace = "checkbox"
-
     class Changed(ToggleButton.Changed):
         """Posted when the value of the checkbox changes."""
 
