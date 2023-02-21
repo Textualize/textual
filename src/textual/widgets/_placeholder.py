dfd67adb60abef3a5b390692d773173896657e66
Will McGugan <willmcgugan@gmail.com>
2023-02-21 13:02:55 +0000

    Invalidate full map

--- src/textual/widgets/_placeholder.py
+++ src/textual/widgets/_placeholder.py
@@ -150,4 +150,4 @@ class Placeholder(Widget):
         """Update the placeholder "size" variant with the new placeholder size."""
         self._renderables["size"] = self._SIZE_RENDER_TEMPLATE.format(*event.size)
         if self.variant == "size":
-            self.refresh(layout=False)
+            self.refresh()
