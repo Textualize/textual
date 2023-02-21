d5f4ae198b1032b224a0151677ec836b1085ae70
Dave Pearson <davep@davep.org>
2023-02-21 21:18:46 +0000

    Have the radio set work more around DOM queries
    Now that I'm toggling a class on the selected buttons.

--- src/textual/widgets/_radio_set.py
+++ src/textual/widgets/_radio_set.py
@@ -3,8 +3,8 @@
 from __future__ import annotations
 
 from ..containers import Container
+from ..css.query import DOMQuery, QueryError
 from ..message import Message
-from ..widget import WidgetError
 from ._radio_button import RadioButton
 from ._toggle import ToggleButton
 
@@ -12,57 +12,47 @@ from ._toggle import ToggleButton
 class RadioSet(Container):
     """Widget for grouping a collection of radio buttons into a set."""
 
-    class ButtonTypeError(WidgetError):
-        """Type of error thrown if an unknown button type is passed."""
-
     def __init__(
         self,
         *buttons: str | RadioButton,
         name: str | None = None,
         id: str | None = None,
         classes: str | None = None,
+        disabled: bool = False,
     ) -> None:
         """Initialise the radio set.
-
         Args:
             buttons: A collection of labels or `RadioButton`s to group together.
             name: The name of the radio set.
             id: The ID of the radio set in the DOM.
             classes: The CSS classes of the radio set.
+            disabled: Whether the radio set is disabled or not.
 
         Note:
             When a `str` label is provided, a `RadioButton` will be created from it.
         """
+        super().__init__(
+            *[
+                (button if isinstance(button, RadioButton) else RadioButton(button))
+                for button in buttons
+            ],
+            name=name,
+            id=id,
+            classes=classes,
+            disabled=disabled,
+        )
 
-        # Be sure we've only been handed strings or radio buttons. This is a
-        # container that can't contain anything else.
-        for button in buttons:
-            if not isinstance(button, (str, RadioButton)):
-                raise self.ButtonTypeError(
-                    f"{button!r} is not of type `str` or `RadioButton`"
-                )
-
-        # Build the internal list of buttons. Here, if we're given a
-        # RadioButton, we use it as-is; otherwise we spin one up from the
-        # given string. We could, of course, just use the children property
-        # to do this as we're guaranteeing that everything within here is a
-        # RadioButton, but doing it this way makes subsequent type issues
-        # less of a problem *and* it's a guard against a developer doing a
-        # sneaky mount on us after the fact.
-        self._buttons = [
-            (button if isinstance(button, RadioButton) else RadioButton(button))
-            for button in buttons
-        ]
-
-        super().__init__(*self._buttons, name=name, id=id, classes=classes)
+    @property
+    def _buttons(self) -> DOMQuery[RadioButton]:
+        """The buttons within the set."""
+        return self.query(RadioButton)
 
     def on_mount(self) -> None:
         """Set up the radio group for use after the DOM is loaded."""
-        # The assumption here is that we should always start out with at
-        # least one button pressed. So, if there isn't a pressed button...
-        if self._buttons and not any(button.value for button in self._buttons):
-            # ...press the first one.
-            self._buttons[0].value = True
+        buttons = self._buttons
+        on_buttons = buttons.filter(".-on")
+        if buttons and not on_buttons:
+            buttons[0].value = True
 
     class Changed(Message, bubble=True):
         """Posted when the pressed button in the set changes."""
@@ -95,14 +85,22 @@ class RadioSet(Container):
             self.post_message_no_wait(self.Changed(self, event.input))
             # ...then look for the button that was previously the pressed
             # one and unpress it.
-            for button in self._buttons:
-                if button.value and button != event.input:
+            for button in self._buttons.filter(".-on"):
+                if button != event.input:
                     button.value = False
                     break
         else:
             # If this leaves us with no buttons checked, disallow that.
-            if not any(button.value for button in self._buttons):
+            if not self._buttons.filter(".-on"):
                 event.input.value = True
+
+    @property
+    def pressed_button(self) -> RadioButton | None:
+        """The currently-pressed button, or `None` none are pressed."""
+        try:
+            return self.query_one("RadioButton.-on", RadioButton)
+        except QueryError:
+            return None
 
     @property
     def pressed_index(self) -> int:
@@ -111,15 +109,7 @@ class RadioSet(Container):
         Note:
             If no button is pressed the value will be `-1`.
         """
-        for index, button in enumerate(self._buttons):
-            if button.value:
-                return index
-        return -1
-
-    @property
-    def pressed_button(self) -> RadioButton | None:
-        """The currently-pressed button, or `None` none are pressed."""
-        for button in self._buttons:
-            if button.value:
-                return button
-        return None
+        try:
+            return self._nodes.index(self.pressed_button)
+        except ValueError:
+            return -1
