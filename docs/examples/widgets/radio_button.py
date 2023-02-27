23c0587efb850f9f993480687463f46effab7152
Dave Pearson <davep@davep.org>
2023-02-27 09:10:10 +0000

    Rework the layout of the radio button example
    Time to start to show off the new compose method.

--- docs/examples/widgets/radio_button.py
+++ docs/examples/widgets/radio_button.py
@@ -6,17 +6,18 @@ class RadioChoicesApp(App[None]):
     CSS_PATH = "radio_button.css"
 
     def compose(self) -> ComposeResult:
-        yield RadioSet(
-            RadioButton("Battlestar Galactica"),
-            RadioButton("Dune 1984"),
-            RadioButton("Dune 2021", id="focus_me"),
-            RadioButton("Serenity", value=True),
-            RadioButton("Star Trek: The Motion Picture"),
-            RadioButton("Star Wars: A New Hope"),
-            RadioButton("The Last Starfighter"),
-            RadioButton("Total Recall :backhand_index_pointing_right: :red_circle:"),
-            RadioButton("Wing Commander"),
-        )
+        with RadioSet():
+            yield RadioButton("Battlestar Galactica")
+            yield RadioButton("Dune 1984")
+            yield RadioButton("Dune 2021", id="focus_me")
+            yield RadioButton("Serenity", value=True)
+            yield RadioButton("Star Trek: The Motion Picture")
+            yield RadioButton("Star Wars: A New Hope")
+            yield RadioButton("The Last Starfighter")
+            yield RadioButton(
+                "Total Recall :backhand_index_pointing_right: :red_circle:"
+            )
+            yield RadioButton("Wing Commander")
 
     def on_mount(self) -> None:
         self.query_one("#focus_me", RadioButton).focus()
