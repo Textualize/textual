f96eb93a601cc4b0ee28411a32b6d3d9cc8ff125
Dave Pearson <davep@davep.org>
2023-03-01 10:11:37 +0000

    Add missing return type to HeaderClock.render

--- src/textual/widgets/_header.py
+++ src/textual/widgets/_header.py
@@ -68,7 +68,7 @@ class HeaderClock(HeaderClockSpace):
     def on_mount(self) -> None:
         self.set_interval(1, callback=self.refresh, name=f"update header clock")
 
-    def render(self):
+    def render(self) -> RenderResult:
         return Text(datetime.now().time().strftime("%X"))
 
 
