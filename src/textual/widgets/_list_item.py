3478615592ebd1daa772a861ee4a4c63feb24332
Dave Pearson <davep@davep.org>
2023-03-01 11:48:13 +0000

    Add a class docstring to ListItem

--- src/textual/widgets/_list_item.py
+++ src/textual/widgets/_list_item.py
@@ -7,6 +7,13 @@ from textual.widget import Widget
 
 
 class ListItem(Widget, can_focus=False):
+    """A widget that is an item within a `ListView`.
+
+    A `ListItem` is designed for use within a
+    [ListView][textual.widgets._list_view.ListView], please see `ListView`'s
+    documentation for more details on use.
+    """
+
     DEFAULT_CSS = """
     ListItem {
         color: $text;
