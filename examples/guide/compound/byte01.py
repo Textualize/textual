ddf10dde7ad24084409e7e494942f8a2c89248b8
Will McGugan <willmcgugan@gmail.com>
2023-03-06 14:37:36 +0000

    Deployed 1e75edbe with MkDocs version: 1.4.2

---
+++ examples/guide/compound/byte01.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+
+from textual.app import App, ComposeResult
+from textual.containers import Container
+from textual.widget import Widget
+from textual.widgets import Input, Label, Switch
+
+
+class BitSwitch(Widget):
+    """A Switch with a numeric label above it."""
+
+    DEFAULT_CSS = """
+    BitSwitch {
+        layout: vertical;
+        width: auto;
+        height: auto;
+    }
+    BitSwitch > Label {
+        text-align: center;
+        width: 1fr;
+    }
+    """
+
+    def __init__(self, bit: int) -> None:
+        self.bit = bit
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        yield Label(str(self.bit))
+        yield Switch()
+
+
+class ByteInput(Widget):
+    """A compound widget with 8 switches."""
+
+    DEFAULT_CSS = """
+    ByteInput {
+        width: auto;
+        height: auto;
+        border: blank;
+        layout: horizontal;
+    }
+    ByteInput:focus-within {
+        border: heavy $secondary;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        for bit in reversed(range(8)):
+            yield BitSwitch(bit)
+
+
+class ByteEditor(Widget):
+    DEFAULT_CSS = """
+    ByteEditor > Container {
+        height: 1fr;
+        align: center middle;
+    }
+    ByteEditor > Container.top {
+        background: $boost;
+    }
+    ByteEditor Input {
+        width: 16;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        with Container(classes="top"):
+            yield Input(placeholder="byte")
+        with Container():
+            yield ByteInput()
+
+
+class ByteInputApp(App):
+    def compose(self) -> ComposeResult:
+        yield ByteEditor()
+
+
+if __name__ == "__main__":
+    app = ByteInputApp()
+    app.run()
---
+++ examples/guide/compound/compound01.py
@@ -0,0 +1,52 @@
+from textual.app import App, ComposeResult
+from textual.widget import Widget
+from textual.widgets import Input, Label
+
+
+class InputWithLabel(Widget):
+    """An input with a label."""
+
+    DEFAULT_CSS = """
+    InputWithLabel {
+        layout: horizontal;
+        height: auto;
+    }
+    InputWithLabel Label {
+        padding: 1;
+        width: 12;
+        text-align: right;
+    }
+    InputWithLabel Input {
+        width: 1fr;
+    }
+    """
+
+    def __init__(self, input_label: str) -> None:
+        self.input_label = input_label
+        super().__init__()
+
+    def compose(self) -> ComposeResult:  # (1)!
+        yield Label(self.input_label)
+        yield Input()
+
+
+class CompoundApp(App):
+    CSS = """
+    Screen {
+        align: center middle;
+    }
+    InputWithLabel {
+        width: 80%;
+        margin: 1;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield InputWithLabel("Fist Name")
+        yield InputWithLabel("Last Name")
+        yield InputWithLabel("Email")
+
+
+if __name__ == "__main__":
+    app = CompoundApp()
+    app.run()
