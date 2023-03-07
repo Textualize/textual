b481c691bf246b65ddfb6fc93da2aa1f655a76db
Dave Pearson <davep@davep.org>
2023-03-07 08:44:07 +0000

    Extend the border snapshot tests
    While this doesn't test *every* permutation, it covers enough bases that if
    something were to change it should catch it.

--- tests/snapshot_tests/snapshot_apps/border_alpha.py
+++ tests/snapshot_tests/snapshot_apps/border_alpha.py
@@ -5,24 +5,39 @@ from textual.widget import Widget
 class BorderAlphaApp(App[None]):
 
     CSS = """
-    Grid { grid-size: 2 5; }
+    Grid {
+        height: 100%;
+        width: 100%;
+        grid-size: 2 2;
+    }
 
-    #b0 { border: solid 0%; }
-    #b1 { border: solid 10%; }
-    #b2 { border: solid 20%; }
-    #b3 { border: solid 30%; }
-    #b4 { border: solid 40%; }
-    #b5 { border: solid 50%; }
-    #b6 { border: solid 60%; }
-    #b7 { border: solid 70%; }
-    #b8 { border: solid 80%; }
-    #b9 { border: solid 90%; }
+    #b00 { border: 0%; }
+    #b01 { border: 33%; }
+    #b02 { border: 66%; }
+    #b03 { border: 100%; }
+
+    #b10 { border: solid 0%; }
+    #b11 { border: dashed 33%; }
+    #b12 { border: round 66%; }
+    #b13 { border: ascii 100%; }
+
+    #b20 { border: 0% red; }
+    #b21 { border: 33% orange; }
+    #b22 { border: 66% green; }
+    #b23 { border: 100% blue; }
+
+    #b30 { border: solid 0% red; }
+    #b31 { border: dashed 33% orange; }
+    #b32 { border: round 66% green; }
+    #b33 { border: ascii 100% blue; }
     """
 
     def compose( self ) -> ComposeResult:
         with Grid():
-            for n in range(10):
-                yield Widget(id=f"b{n}")
+            for outer in range(4):
+                with Grid():
+                    for inner in range(4):
+                        yield Widget(id=f"b{outer}{inner}")
 
 if __name__ == "__main__":
     BorderAlphaApp().run()
