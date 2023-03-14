27313e7f8117ad2a01e11605f7c9ac6c68d28e0b
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-03-14 15:06:02 +0000

    Test setting border_(sub)title.

--- tests/test_border.py
+++ tests/test_border.py
@@ -2,6 +2,7 @@ from rich.segment import Segment
 from rich.style import Style
 
 from textual._border import render_row
+from textual.widget import Widget
 
 
 def test_border_render_row():
@@ -22,3 +23,43 @@ def test_border_render_row():
         Segment(row[1].text * 3, row[1].style),
         row[2],
     ]
+
+
+def test_border_title_single_line():
+    """The border_title gets set to a single line even when multiple lines are provided."""
+    widget = Widget()
+
+    widget.border_title = ""
+    assert widget.border_title == ""
+
+    widget.border_title = "How is life\ngoing for you?"
+    assert widget.border_title == "How is life"
+
+    widget.border_title = "How is life\n\rgoing for you?"
+    assert widget.border_title == "How is life"
+
+    widget.border_title = "Sorry you \r\n have to \n read this."
+    assert widget.border_title == "Sorry you "
+
+    widget.border_title = "[red]This also \n works with markup \n involved.[/]"
+    assert widget.border_title == "[red]This also "
+
+
+def test_border_subtitle_single_line():
+    """The border_subtitle gets set to a single line even when multiple lines are provided."""
+    widget = Widget()
+
+    widget.border_subtitle = ""
+    assert widget.border_subtitle == ""
+
+    widget.border_subtitle = "How is life\ngoing for you?"
+    assert widget.border_subtitle == "How is life"
+
+    widget.border_subtitle = "How is life\n\rgoing for you?"
+    assert widget.border_subtitle == "How is life"
+
+    widget.border_subtitle = "Sorry you \r\n have to \n read this."
+    assert widget.border_subtitle == "Sorry you "
+
+    widget.border_subtitle = "[red]This also \n works with markup \n involved.[/]"
+    assert widget.border_subtitle == "[red]This also "
