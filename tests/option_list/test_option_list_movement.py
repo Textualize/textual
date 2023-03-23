1c0e7017b1911b738ab53f67cbd7d4a4429af918
Dave Pearson <davep@davep.org>
2023-03-23 10:15:58 +0000

    Add some more OptionList movement tests

--- tests/option_list/test_option_list_movement.py
+++ tests/option_list/test_option_list_movement.py
@@ -27,12 +27,30 @@ async def test_cleared_highlight_is_none() -> None:
         assert option_list.highlighted is None
 
 
+async def test_cleared_movement_does_nothing() -> None:
+    """The highlight should remain `None` if the list is cleared."""
+    async with OptionListApp().run_test() as pilot:
+        option_list = pilot.app.query_one(OptionList)
+        option_list.clear()
+        assert option_list.highlighted is None
+        await pilot.press("tab", "down", "up", "page_down", "page_up", "home", "end")
+        assert option_list.highlighted is None
+
+
 async def test_move_down() -> None:
     """The highlight should move down when asked to."""
     async with OptionListApp().run_test() as pilot:
         await pilot.press("tab", "down")
+        assert pilot.app.query_one(OptionList).highlighted == 1
+
+
+async def test_move_down_from_end() -> None:
+    """The highlight should wrap around when moving down from the end."""
+    async with OptionListApp().run_test() as pilot:
         option_list = pilot.app.query_one(OptionList)
-        assert option_list.highlighted == 1
+        option_list.highlighted = 5
+        await pilot.press("tab", "down")
+        assert option_list.highlighted == 0
 
 
 async def test_move_up() -> None:
@@ -44,4 +62,26 @@ async def test_move_up() -> None:
         assert option_list.highlighted == 0
 
 
-# TODO: MORE
+async def test_move_up_from_nowhere() -> None:
+    """The highlight should settle on the last item when moving up from `None`."""
+    async with OptionListApp().run_test() as pilot:
+        await pilot.press("tab", "up")
+        assert pilot.app.query_one(OptionList).highlighted == 5
+
+
+async def test_move_end() -> None:
+    """The end key should go to the end of the list."""
+    async with OptionListApp().run_test() as pilot:
+        await pilot.press("tab", "end")
+        assert pilot.app.query_one(OptionList).highlighted == 5
+
+
+async def test_move_home() -> None:
+    """The home key should go to the start of the list."""
+    async with OptionListApp().run_test() as pilot:
+        option_list = pilot.app.query_one(OptionList)
+        assert option_list.highlighted == 0
+        option_list.highlighted = 5
+        assert option_list.highlighted == 5
+        await pilot.press("tab", "home")
+        assert option_list.highlighted == 0
