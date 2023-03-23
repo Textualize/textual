72ad71a9b18296129f88c038322f022262679357
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-03-23 11:55:12 +0000

    Simplify action parsing.
    Turns out that we can just wrap the string that we want to parse as the arguments in '({string_here},)', as per @willmcgugan's comment in the PR review.
    
    Related review comments: https://github.com/Textualize/textual/pull/2112\#issuecomment-1481015988

--- src/textual/actions.py
+++ src/textual/actions.py
@@ -20,46 +20,6 @@ class ActionError(Exception):
 re_action_args = re.compile(r"([\w\.]+)\((.*)\)")
 
 
-def _is_single_tuple_argument(evals_to_tuple: str) -> bool:
-    """Is this tuple a single argument tuple?
-
-    If `evals_to_tuple` is such that `ast.literal_eval` evaluates it to a tuple,
-    is that because it is a comma-separated list of values, like `"1, 2, (False, 3)"`,
-    or is it because it is a single tuple, like `"(1, 2, False)"`?
-
-    If it is a single tuple, the string must start with "(", must end with ")", and after
-    removing those two parenthesis, `ast.literal_eval` must still be able to parse it
-    into the same valid tuple.
-
-    This function must also be able to handle strings like `"(1, 2), (3, 4)"`, which is
-    a 2-item tuple in a string that starts with "(" and ends with ")", but that
-    represents two arguments for our action.
-
-    Args:
-        evals_to_tuple: A string representing action arguments and that is of the type
-            `tuple` when evaluated by `ast.literal_eval`.
-
-    Returns:
-        Whether the string represents a single tuple or a space-separated list of
-            arguments.
-    """
-    if not evals_to_tuple.startswith("(") or not evals_to_tuple.endswith(")"):
-        return False
-
-    without_outer_parens = evals_to_tuple[1:-1]
-    # Short-circuit the case `evals_to_tuple == "()"` because `ast.literal_eval` will
-    # raise a SyntaxError (EOF) when evaluating `""`.
-    if not without_outer_parens:
-        return True
-
-    try:
-        ast.literal_eval(evals_to_tuple[1:-1])
-    except SyntaxError:
-        return False
-    else:
-        return True
-
-
 def parse(action: str) -> ActionParseResult:
     """Parses an action string.
 
@@ -77,26 +37,14 @@ def parse(action: str) -> ActionParseResult:
         action_name, action_args_str = args_match.groups()
         if action_args_str:
             try:
-                tentative_action_args: tuple[Any, ...] | Any = ast.literal_eval(
-                    action_args_str
-                )
+                # We wrap `action_args_str` to be able to disambiguate the cases where
+                # the list of arguments is a comma-separated list of values from the
+                # case where the argument is a single tuple.
+                action_args: tuple[Any, ...] = ast.literal_eval(f"({action_args_str},)")
             except Exception:
                 raise ActionError(
                     f"unable to parse {action_args_str!r} in action {action!r}"
                 )
-            else:
-                # At this point, if tentative_action_args is not a tuple, it was a
-                # single argument. If tentative_action_args IS a tuple, it may have
-                # been because the tentative_action_args was a comma-separated list
-                # of arguments, like `"1, False, (1, 2, 3)"`, or because it was a single
-                # argument tuple, like `"(1, 2, 3)"`. In the latter case,
-                # we need to convert to `((1, 2, 3),)`.
-                if isinstance(
-                    tentative_action_args, tuple
-                ) and not _is_single_tuple_argument(action_args_str):
-                    action_args = tentative_action_args
-                else:
-                    action_args = (tentative_action_args,)
         else:
             action_args = ()
     else:
