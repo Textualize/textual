74a2d079b3053263b39f5620ae5438dc5ef0322c
Dave Pearson <davep@davep.org>
2023-03-29 09:41:24 +0100

    Update the mouse hover test after the changes to the tracking variable

--- tests/option_list/test_option_list_mouse_hover.py
+++ tests/option_list/test_option_list_mouse_hover.py
@@ -19,20 +19,20 @@ async def test_no_hover() -> None:
     """When the mouse isn't over the OptionList mouse_hovering_over should be None."""
     async with OptionListApp().run_test() as pilot:
         await pilot.hover(Label)
-        assert pilot.app.query_one(OptionList).mouse_hovering_over is None
+        assert pilot.app.query_one(OptionList)._mouse_hovering_over is None
 
 
 async def test_hover() -> None:
     """The mouse hover reactive should react to the mouse hover."""
     async with OptionListApp().run_test() as pilot:
         await pilot.hover(OptionList)
-        assert pilot.app.query_one(OptionList).mouse_hovering_over == 0
+        assert pilot.app.query_one(OptionList)._mouse_hovering_over == 0
 
 
 async def test_hover_then_leave() -> None:
     """After a mouse has been over an OptionList and left mouse_hovering_over should be None again."""
     async with OptionListApp().run_test() as pilot:
         await pilot.hover(OptionList)
-        assert pilot.app.query_one(OptionList).mouse_hovering_over == 0
+        assert pilot.app.query_one(OptionList)._mouse_hovering_over == 0
         await pilot.hover(Label)
-        assert pilot.app.query_one(OptionList).mouse_hovering_over is None
+        assert pilot.app.query_one(OptionList)._mouse_hovering_over is None
