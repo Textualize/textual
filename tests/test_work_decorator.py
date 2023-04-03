193440a69eeafbfc13085587bb630cbbd7e09816
Will McGugan <willmcgugan@gmail.com>
2023-04-03 17:56:01 +0100

    worker test

--- tests/test_work_decorator.py
+++ tests/test_work_decorator.py
@@ -8,26 +8,26 @@ from textual.worker import Worker, WorkerState
 async def test_work() -> None:
     """Test basic usage of the @work decorator."""
     states: list[WorkerState] = []
-    worker: Worker[str] | None = None
 
     class WorkApp(App):
+        worker: Worker
+
         @work
         async def foo(self) -> str:
             await asyncio.sleep(0.1)
             return "foo"
 
         def on_mount(self) -> None:
-            nonlocal worker
-            worker = self.foo()
+            self.worker = self.foo()
 
         def on_worker_state_changed(self, event: Worker.StateChanged) -> None:
             states.append(event.state)
 
     app = WorkApp()
+
     async with app.run_test() as pilot:
-        assert isinstance(worker, Worker)
         await app.workers.wait_for_complete()
-        result = await worker.wait()
+        result = await app.worker.wait()
         assert result == "foo"
         await pilot.pause()
     assert states == [WorkerState.PENDING, WorkerState.RUNNING, WorkerState.SUCCESS]
