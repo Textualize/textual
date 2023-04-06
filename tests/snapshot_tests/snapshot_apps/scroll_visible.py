552de87caa034e4cd982ee7b7633e2b2d4ee32b5
Will McGugan <willmcgugan@gmail.com>
2023-04-06 10:31:01 +0100

    scroll visible snapshot

---
+++ tests/snapshot_tests/snapshot_apps/scroll_visible.py
@@ -0,0 +1,22 @@
+from textual.app import App, ComposeResult
+from textual.containers import VerticalScroll
+from textual.widgets import Label, Static
+
+
+class MyCustomWidget(Static):
+    def compose(self) -> ComposeResult:
+        yield Label(("|\n" * 100)[:-1])
+        yield Label("CAN'T GET HERE", id="target")
+
+
+class MyApp(App):
+    def compose(self) -> ComposeResult:
+        with VerticalScroll():
+            yield MyCustomWidget()
+
+    def key_t(self) -> None:
+        self.query_one("#target").scroll_visible()
+
+
+if __name__ == "__main__":
+    MyApp().run()
