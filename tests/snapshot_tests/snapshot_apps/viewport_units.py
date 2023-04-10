7ce7334fba3d029885f1b0c289a6e69a98317109
Darren Burns <darrenb900@gmail.com>
2023-04-10 15:08:12 +0100

    Fix snapshot app

--- tests/snapshot_tests/snapshot_apps/viewport_units.py
+++ tests/snapshot_tests/snapshot_apps/viewport_units.py
@@ -3,7 +3,7 @@ from textual.widgets import Static
 
 
 class ViewportUnits(App):
-    CSS = """Static {width: 100vh; height: 100vh; border: solid cyan;} """
+    CSS = """Static {width: 100vw; height: 100vh; border: solid cyan;} """
 
     def compose(self) -> ComposeResult:
         yield Static("Hello, world!")
