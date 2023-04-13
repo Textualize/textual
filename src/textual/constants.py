fa05e169e307187974b13879b88aba7b1a474e73
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-04-13 11:10:36 +0100

    Factor logic into function.
    Related review: https://github.com/Textualize/textual/pull/2258\#discussion_r1165298259

--- src/textual/constants.py
+++ src/textual/constants.py
@@ -6,6 +6,7 @@ Constants that we might want to expose via the public API.
 from __future__ import annotations
 
 import os
+from typing import overload
 
 from typing_extensions import Final
 
@@ -29,6 +30,24 @@ def get_environ_bool(name: str) -> bool:
     return has_environ
 
 
+def get_environ_int(name: str, default: int) -> int:
+    """Retrieves an integer environment variable.
+
+    Args:
+        name: Name of environment variable.
+        default: The value to use if the value is not set, or set to something other
+            than a valid integer.
+
+    Returns:
+        The integer associated with the environment variable if it's set to a valid int
+            or the default value otherwise.
+    """
+    try:
+        return int(get_environ(name, default))
+    except ValueError:
+        return default
+
+
 BORDERS = list(BORDER_CHARS)
 
 DEBUG: Final[bool] = get_environ_bool("TEXTUAL_DEBUG")
@@ -39,10 +58,11 @@ LOG_FILE: Final[str | None] = get_environ("TEXTUAL_LOG", None)
 """A last resort log file that appends all logs, when devtools isn't working."""
 
 
-DEVTOOLS_PORT_ENVIRON_VARIABLE = "TEXTUAL_CONSOLE_PORT"
-DEFAULT_DEVTOOLS_PORT = 8081
-try:
-    _devtools_port = int(os.environ[DEVTOOLS_PORT_ENVIRON_VARIABLE])
-except (KeyError, ValueError):
-    _devtools_port = DEFAULT_DEVTOOLS_PORT
-DEVTOOLS_PORT: Final[int] = _devtools_port
+DEVTOOLS_PORT_ENVIRON_VARIABLE: Final[str] = "TEXTUAL_CONSOLE_PORT"
+"""The name of the environment variable that sets the port for the devtools."""
+DEFAULT_DEVTOOLS_PORT: Final[int] = 8081
+"""The default port to use for the devtools."""
+DEVTOOLS_PORT: Final[int] = get_environ_int(
+    DEVTOOLS_PORT_ENVIRON_VARIABLE, DEFAULT_DEVTOOLS_PORT
+)
+"""Constant with the port that the devtools will connect to."""
