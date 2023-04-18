b573d0a04d139d671bfabc657a1036ce395ebc18
Will McGugan <willmcgugan@gmail.com>
2023-04-18 21:40:03 +0100

    dim filter

--- src/textual/filter.py
+++ src/textual/filter.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from functools import lru_cache
 
+from rich.color import Color as RichColor
 from rich.segment import Segment
 from rich.style import Style
 
@@ -50,3 +51,73 @@ class Monochrome(LineFilter):
             else Color.from_rich_color(style_background).monochrome.rich_color
         )
         return style + Style.from_color(color, background)
+
+
+NO_DIM = Style(dim=False)
+"""A Style to set dim to False."""
+
+
+# Can be used as a workaround for https://github.com/xtermjs/xterm.js/issues/4161
+class DimFilter(LineFilter):
+    """Replace dim attributes with modified colors."""
+
+    def __init__(self, dim_factor: float = 0.5) -> None:
+        """Initialize the filter.
+
+        Args:
+            dim_factor: The factor to dim by; 0 is 100% background (i.e. invisible), 1.0 is no change.
+        """
+        self.dim_factor = dim_factor
+
+    @lru_cache(1024)
+    def dim_color(self, background: RichColor, color: RichColor) -> RichColor:
+        """Dim a color by blending towards the background."""
+        return (
+            Color.from_rich_color(background)
+            .blend(Color.from_rich_color(color), self.dim_factor)
+            .rich_color
+        )
+
+    @lru_cache(1024)
+    def dim_style(self, style: Style) -> Style:
+        """Replace dim attribute with a dim color"""
+        return (
+            style
+            + Style.from_color(color=self.dim_color(style.bgcolor, style.color))
+            + NO_DIM
+        )
+
+    def apply(self, segments: list[Segment]) -> list[Segment]:
+        """Modify color of segments with dim style."""
+        _Segment = Segment
+        dim_style = self.dim_style
+
+        return [
+            (
+                _Segment(
+                    segment.text,
+                    dim_style(segment.style),
+                    None,
+                )
+                if segment.style is not None and segment.style.dim
+                else segment
+            )
+            for segment in segments
+        ]
+
+
+if __name__ == "__main__":
+    from rich.segment import Segments
+    from rich.text import Text
+
+    text = Text.from_markup("[dim #ffffff on #0000ff]Hello World!")
+
+    from rich.console import Console
+
+    console = Console()
+
+    segments = list(text.render(console))
+    console.print(Segments(segments))
+    console.print()
+    filter = DimFilter().apply
+    console.print(Segments(filter(segments)))
---
+++ tests/test_line_filter.py
@@ -0,0 +1,18 @@
+from rich.segment import Segment
+from rich.style import Style
+
+from textual.filter import DimFilter
+
+
+def test_dim_apply():
+    """Check dim filter changes color and resets dim attribute."""
+
+    dim_filter = DimFilter()
+
+    segments = [Segment("Hello, World!", Style.parse("dim #ffffff on #0000ff"))]
+
+    dimmed_segments = dim_filter.apply(segments)
+
+    expected = [Segment("Hello, World!", Style.parse("not dim #7f7fff on #0000ff"))]
+
+    assert dimmed_segments == expected
