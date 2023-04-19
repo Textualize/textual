9123a80aa54c452042c128c1dd43ced5b1ebf7b3
Dave Pearson <davep@davep.org>
2023-04-19 11:39:19 +0100

    Update a screen instance's callback stack when it is switched out
    As per the observation here:
    
      https://github.com/Textualize/textual/pull/2321#pullrequestreview-1391744148

--- src/textual/app.py
+++ src/textual/app.py
@@ -1423,7 +1423,8 @@ class App(Generic[ReturnType], DOMNode):
                 f"switch_screen requires a Screen instance or str; not {screen!r}"
             )
         if self.screen is not screen:
-            self._replace_screen(self._screen_stack.pop())
+            previous_screen = self._replace_screen(self._screen_stack.pop())
+            previous_screen._pop_result_callback()
             next_screen, await_mount = self._get_screen(screen)
             self._screen_stack.append(next_screen)
             self.screen.post_message(events.ScreenResume())
