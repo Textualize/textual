3bbd61d42e8857e39514693bf9fc2fdd01d634da
Will McGugan <willmcgugan@gmail.com>
2023-04-20 16:19:43 +0100

    guard against bad imports

--- src/textual/cli/_run.py
+++ src/textual/cli/_run.py
@@ -9,6 +9,7 @@ This means that (if they succeed) they will never return.
 
 from __future__ import annotations
 
+import importlib
 import os
 import platform
 import shlex
@@ -32,6 +33,10 @@ else:
 """
 )
 """A template script to import and run an app."""
+
+
+class ExecImportError(Exception):
+    """Raised if a Python import is invalid."""
 
 
 def run_app(command_args: str, environment: dict[str, str] | None = None) -> None:
@@ -59,7 +64,7 @@ def run_app(command_args: str, environment: dict[str, str] | None = None) -> Non
         # Otherwise it is assumed to be a Python import
         try:
             exec_import(import_name, args, app_environment)
-        except SyntaxError:
+        except (SyntaxError, ExecImportError):
             print(f"Unable to import Textual app from {import_name!r}")
 
 
@@ -112,6 +117,25 @@ def exec_command(command: str, environment: dict[str, str]) -> NoReturn:
     os.execvpe(command, [command, *args], environment)
 
 
+def check_import(module_name: str, app_name: str) -> bool:
+    """Check if a symbol can be imported.
+
+    Args:
+        module_name: Name of the module
+        app_name: Name of the app.
+
+    Returns:
+        True if the app may be imported from the module.
+    """
+
+    try:
+        sys.path.insert(0, "")
+        module = importlib.import_module(module_name)
+    except ImportError as error:
+        return False
+    return hasattr(module, app_name)
+
+
 def exec_import(
     import_name: str, args: list[str], environment: dict[str, str]
 ) -> NoReturn:
@@ -119,6 +143,7 @@ def exec_import(
 
     Raises:
         SyntaxError: If any imports are not valid Python symbols.
+        ExecImportError: If the module could not be imported.
 
     Args:
         import_name: The Python import.
@@ -127,7 +152,12 @@ def exec_import(
 
     """
     module, _colon, app = import_name.partition(":")
-    script = EXEC_SCRIPT.substitute(MODULE=module, APP=app or "app")
+    app = app or "app"
+
+    if not check_import(module, app):
+        raise ExecImportError(f"Unable to import {app!r} from  {import_name!r}")
+
+    script = EXEC_SCRIPT.substitute(MODULE=module, APP=app)
     # Compiling the script will raise a SyntaxError if there are any invalid symbols
     compile(script, "textual-exec", "exec")
     _flush()
