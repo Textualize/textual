bc3ffc4aafc794a12ea8fe9cd46571c75493f90d
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-04-20 14:04:10 +0100

    Repurpose UnderlineBar.

--- tests/renderables/test_underline_bar.py
+++ tests/renderables/test_underline_bar.py
@@ -4,7 +4,7 @@ from rich.console import Console, ConsoleOptions
 from rich.text import Text
 
 from tests.utilities.render import render
-from textual.renderables.underline_bar import UnderlineBar
+from textual.renderables.bar import RenderableBar
 
 MAGENTA = "\x1b[35m"
 GREY = "\x1b[38;5;59m"
@@ -14,22 +14,22 @@ RED = "\x1b[31m"
 
 
 def test_no_highlight():
-    bar = UnderlineBar(width=6)
+    bar = RenderableBar(width=6)
     assert render(bar) == f"{GREY}━━━━━━{STOP}"
 
 
 def test_highlight_from_zero():
-    bar = UnderlineBar(highlight_range=(0, 2.5), width=6)
+    bar = RenderableBar(highlight_range=(0, 2.5), width=6)
     assert render(bar) == (f"{MAGENTA}━━{STOP}{MAGENTA}╸{STOP}{GREY}━━━{STOP}")
 
 
 def test_highlight_from_zero_point_five():
-    bar = UnderlineBar(highlight_range=(0.5, 2), width=6)
+    bar = RenderableBar(highlight_range=(0.5, 2), width=6)
     assert render(bar) == (f"{MAGENTA}╺━{STOP}{GREY}╺{STOP}{GREY}━━━{STOP}")
 
 
 def test_highlight_middle():
-    bar = UnderlineBar(highlight_range=(2, 4), width=6)
+    bar = RenderableBar(highlight_range=(2, 4), width=6)
     assert render(bar) == (
         f"{GREY}━{STOP}"
         f"{GREY}╸{STOP}"
@@ -40,14 +40,14 @@ def test_highlight_middle():
 
 
 def test_highlight_half_start():
-    bar = UnderlineBar(highlight_range=(2.5, 4), width=6)
+    bar = RenderableBar(highlight_range=(2.5, 4), width=6)
     assert render(bar) == (
         f"{GREY}━━{STOP}" f"{MAGENTA}╺━{STOP}" f"{GREY}╺{STOP}" f"{GREY}━{STOP}"
     )
 
 
 def test_highlight_half_end():
-    bar = UnderlineBar(highlight_range=(2, 4.5), width=6)
+    bar = RenderableBar(highlight_range=(2, 4.5), width=6)
     assert render(bar) == (
         f"{GREY}━{STOP}"
         f"{GREY}╸{STOP}"
@@ -58,46 +58,46 @@ def test_highlight_half_end():
 
 
 def test_highlight_half_start_and_half_end():
-    bar = UnderlineBar(highlight_range=(2.5, 4.5), width=6)
+    bar = RenderableBar(highlight_range=(2.5, 4.5), width=6)
     assert render(bar) == (
         f"{GREY}━━{STOP}" f"{MAGENTA}╺━{STOP}" f"{MAGENTA}╸{STOP}" f"{GREY}━{STOP}"
     )
 
 
 def test_highlight_to_near_end():
-    bar = UnderlineBar(highlight_range=(3, 5.5), width=6)
+    bar = RenderableBar(highlight_range=(3, 5.5), width=6)
     assert render(bar) == (
         f"{GREY}━━{STOP}" f"{GREY}╸{STOP}" f"{MAGENTA}━━{STOP}" f"{MAGENTA}╸{STOP}"
     )
 
 
 def test_highlight_to_end():
-    bar = UnderlineBar(highlight_range=(3, 6), width=6)
+    bar = RenderableBar(highlight_range=(3, 6), width=6)
     assert render(bar) == (f"{GREY}━━{STOP}{GREY}╸{STOP}{MAGENTA}━━━{STOP}")
 
 
 def test_highlight_out_of_bounds_start():
-    bar = UnderlineBar(highlight_range=(-2, 3), width=6)
+    bar = RenderableBar(highlight_range=(-2, 3), width=6)
     assert render(bar) == (f"{MAGENTA}━━━{STOP}{GREY}╺{STOP}{GREY}━━{STOP}")
 
 
 def test_highlight_out_of_bounds_end():
-    bar = UnderlineBar(highlight_range=(3, 9), width=6)
+    bar = RenderableBar(highlight_range=(3, 9), width=6)
     assert render(bar) == (f"{GREY}━━{STOP}{GREY}╸{STOP}{MAGENTA}━━━{STOP}")
 
 
 def test_highlight_full_range_out_of_bounds_end():
-    bar = UnderlineBar(highlight_range=(9, 10), width=6)
+    bar = RenderableBar(highlight_range=(9, 10), width=6)
     assert render(bar) == f"{GREY}━━━━━━{STOP}"
 
 
 def test_highlight_full_range_out_of_bounds_start():
-    bar = UnderlineBar(highlight_range=(-5, -2), width=6)
+    bar = RenderableBar(highlight_range=(-5, -2), width=6)
     assert render(bar) == f"{GREY}━━━━━━{STOP}"
 
 
 def test_custom_styles():
-    bar = UnderlineBar(
+    bar = RenderableBar(
         highlight_range=(2, 4), highlight_style="red", background_style="green", width=6
     )
     assert render(bar) == (
@@ -110,7 +110,7 @@ def test_custom_styles():
 
 
 def test_clickable_ranges():
-    bar = UnderlineBar(
+    bar = RenderableBar(
         highlight_range=(0, 1), width=6, clickable_ranges={"foo": (0, 2), "bar": (4, 5)}
     )
 
