0dc89dc2a807c0a8ca81a8f2632e308dcbaff7ec
Will McGugan <willmcgugan@gmail.com>
2023-05-10 22:25:04 +0200

    wrong axis

--- src/textual/geometry.py
+++ src/textual/geometry.py
@@ -839,7 +839,7 @@ class Region(NamedTuple):
 
     @lru_cache(maxsize=1024)
     def split_horizontal(self, cut: int) -> tuple[Region, Region]:
-        """Split a region in to two, from a given x offset.
+        """Split a region in to two, from a given y offset.
 
         ```
                     ┌─────────┐
