58a9cb19092846d592d958b9845bbe8678373d54
Will McGugan <willmcgugan@gmail.com>
2023-06-01 11:33:54 +0100

    blog post new release (#2712)
    * blog post new release
    
    * update words
    
    * Update docs/blog/posts/release0-27-0.md
    
    Co-authored-by: Dave Pearson <davep@davep.org>
    
    ---------
    
    Co-authored-by: Dave Pearson <davep@davep.org>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,8 +5,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## Unreleased
-
+## [0.27.0] - 2023-06-01
 
 ### Fixed
 
--- docs/examples/widgets/sparkline_colors.py
+++ docs/examples/widgets/sparkline_colors.py
@@ -1,3 +1,5 @@
+from math import sin
+
 from textual.app import App, ComposeResult
 from textual.widgets._sparkline import Sparkline
 
@@ -6,7 +8,7 @@ class SparklineColorsApp(App[None]):
     CSS_PATH = "sparkline_colors.css"
 
     def compose(self) -> ComposeResult:
-        nums = [10, 2, 30, 60, 45, 20, 7, 8, 9, 10, 50, 13, 10, 6, 5, 4, 3, 7, 20]
+        nums = [abs(sin(x / 3.14)) for x in range(0, 360 * 6, 20)]
         yield Sparkline(nums, summary_function=max, id="fst")
         yield Sparkline(nums, summary_function=max, id="snd")
         yield Sparkline(nums, summary_function=max, id="trd")
