8c46884aad28fd62b01a865992a29032d1e528ea
Darren Burns <darrenb900@gmail.com>
2023-06-05 10:55:03 +0100

    Add snapshot for style ordering in the DataTable

--- tests/snapshot_tests/snapshot_apps/data_table_style_order.py
+++ tests/snapshot_tests/snapshot_apps/data_table_style_order.py
@@ -1,14 +1,10 @@
 from textual.app import App, ComposeResult
-from textual.widgets import DataTable
+from textual.widgets import DataTable, Label
 
 data = [
     "Severance",
     "Foundation",
     "Dark",
-    "The Boys",
-    "The Last of Us",
-    "Lost in Space",
-    "Altered Carbon",
 ]
 
 
@@ -20,18 +16,37 @@ class DataTableCursorStyles(App):
     on top."""
 
     CSS = """
+    DataTable {margin-bottom: 1;}
 DataTable > .datatable--cursor {
-    color: $text;
+    color: $secondary;
     background: $success;
     text-style: bold italic;
 }
 """
 
     def compose(self) -> ComposeResult:
-        table = DataTable()
+        priorities = [
+            ("css", "css"),
+            ("css", "renderable"),
+            ("renderable", "renderable"),
+            ("renderable", "css"),
+        ]
+        for foreground, background in priorities:
+            yield Label(f"Foreground is {foreground!r}, background is {background!r}:")
+            table = self.make_datatable(foreground, background)
+            yield table
+
+    def make_datatable(self, foreground_priority, background_priority) -> DataTable:
+        table = DataTable(cursor_foreground_priority=foreground_priority,
+                          cursor_background_priority=background_priority)
         table.zebra_stripes = True
         table.add_column("Movies")
         for row in data:
-            table.add_row(f"[red]{row}")
-        table.focus()
-        yield table
+            table.add_row(f"[red on blue]{row}")
+        return table
+
+
+app = DataTableCursorStyles()
+
+if __name__ == '__main__':
+    app.run()
