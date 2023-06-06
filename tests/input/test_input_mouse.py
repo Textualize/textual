85cc741aa3def9f22e500e2e0c63ae4c9d87c3e7
Dave Pearson <davep@davep.org>
2023-06-06 08:04:33 +0100

    Tidy up the Input mouse tests
    Also add a test for clicking outside of the content.

--- tests/input/test_input_mouse.py
+++ tests/input/test_input_mouse.py
@@ -12,22 +12,29 @@ class InputApp(App[None]):
         yield Input(self.TEST_TEXT)
 
 
-async def test_mouse_clicks():
+async def test_mouse_clicks_within():
     """Mouse clicks should result in the cursor going to the right place."""
-    clicks = (
-        (0, 0),
-        (1, 1),
-        (len(InputApp.TEST_TEXT) - 1, len(InputApp.TEST_TEXT) - 1),
-        (len(InputApp.TEST_TEXT), len(InputApp.TEST_TEXT)),
-        (-5, len(InputApp.TEST_TEXT)),
-        (len(InputApp.TEST_TEXT) * 2, len(InputApp.TEST_TEXT)),
-    )
     async with InputApp().run_test() as pilot:
-        for click_at, should_land in clicks:
+        for click_at, should_land in (
+            (0, 0),
+            (1, 1),
+            (10, 10),
+            (len(InputApp.TEST_TEXT) - 1, len(InputApp.TEST_TEXT) - 1),
+            (len(InputApp.TEST_TEXT), len(InputApp.TEST_TEXT)),
+            (len(InputApp.TEST_TEXT) * 2, len(InputApp.TEST_TEXT)),
+        ):
+            # Note the offsets to take into account the decoration around an
+            # Input.
             await pilot.click(Input, Offset(click_at + 3, 1))
             await pilot.pause()
             assert pilot.app.query_one(Input).cursor_position == should_land
 
 
-if __name__ == "__main__":
-    InputApp().run()
+async def test_mouse_click_outwith():
+    """Mouse clicks outside the input should not affect cursor position."""
+    async with InputApp().run_test() as pilot:
+        pilot.app.query_one(Input).cursor_position = 3
+        assert pilot.app.query_one(Input).cursor_position == 3
+        await pilot.click(Input, Offset(0, 0))
+        await pilot.pause()
+        assert pilot.app.query_one(Input).cursor_position == 3
