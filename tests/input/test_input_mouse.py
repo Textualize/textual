5ca6d54973472d5155ecac6c4a86eda9a9bb1b6f
Dave Pearson <davep@davep.org>
2023-06-06 10:17:45 +0100

    Switch clicks_within to use parametrize
    TIL.

--- tests/input/test_input_mouse.py
+++ tests/input/test_input_mouse.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import pytest
+
 from textual.app import App, ComposeResult
 from textual.geometry import Offset
 from textual.widgets import Input
@@ -12,22 +14,25 @@ class InputApp(App[None]):
         yield Input(self.TEST_TEXT)
 
 
-async def test_mouse_clicks_within():
+@pytest.mark.parametrize(
+    "click_at, should_land",
+    (
+        (0, 0),
+        (1, 1),
+        (10, 10),
+        (len(InputApp.TEST_TEXT) - 1, len(InputApp.TEST_TEXT) - 1),
+        (len(InputApp.TEST_TEXT), len(InputApp.TEST_TEXT)),
+        (len(InputApp.TEST_TEXT) * 2, len(InputApp.TEST_TEXT)),
+    ),
+)
+async def test_mouse_clicks_within(click_at, should_land):
     """Mouse clicks should result in the cursor going to the right place."""
     async with InputApp().run_test() as pilot:
-        for click_at, should_land in (
-            (0, 0),
-            (1, 1),
-            (10, 10),
-            (len(InputApp.TEST_TEXT) - 1, len(InputApp.TEST_TEXT) - 1),
-            (len(InputApp.TEST_TEXT), len(InputApp.TEST_TEXT)),
-            (len(InputApp.TEST_TEXT) * 2, len(InputApp.TEST_TEXT)),
-        ):
-            # Note the offsets to take into account the decoration around an
-            # Input.
-            await pilot.click(Input, Offset(click_at + 3, 1))
-            await pilot.pause()
-            assert pilot.app.query_one(Input).cursor_position == should_land
+        # Note the offsets to take into account the decoration around an
+        # Input.
+        await pilot.click(Input, Offset(click_at + 3, 1))
+        await pilot.pause()
+        assert pilot.app.query_one(Input).cursor_position == should_land
 
 
 async def test_mouse_click_outwith():
