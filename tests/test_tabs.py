4f8936f4e4968daf48ea6441d9c5fb972b3d96ef
Dave Pearson <davep@davep.org>
2023-06-14 09:21:55 +0100

    Add a test for changing the active tab from code

--- tests/test_tabs.py
+++ tests/test_tabs.py
@@ -121,3 +121,33 @@ async def test_clear_tabs():
         await tabs.clear()
         assert tabs.tab_count == 0
         assert tabs.active_tab is None
+
+
+async def test_change_active_from_code():
+    """It should be possible to change the active tab from code.."""
+
+    class TabsApp(App[None]):
+        def compose(self) -> ComposeResult:
+            yield Tabs("John", "Aeryn", "Moya", "Pilot")
+
+    async with TabsApp().run_test() as pilot:
+        tabs = pilot.app.query_one(Tabs)
+        assert tabs.tab_count == 4
+        assert tabs.active_tab is not None
+        assert tabs.active_tab.id == "tab-1"
+        assert tabs.active == tabs.active_tab.id
+
+        tabs.active = "tab-2"
+        assert tabs.active_tab is not None
+        assert tabs.active_tab.id == "tab-2"
+        assert tabs.active == tabs.active_tab.id
+
+        # TODO: This one is questionable. It seems Tabs has been designed so
+        # that you can set the active tab to an empty string, and it remains
+        # so, and just removes the underline; no other changes. So active
+        # will be an empty string while active_tab will be a tab. This feels
+        # like an oversight. Need to investigate and possibly modify this
+        # behaviour unless there's a good reason for this.
+        tabs.active = ""
+        assert tabs.active_tab is not None
+        assert tabs.active_tab.id == "tab-2"
