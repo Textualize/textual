ade3548395513578dd7ad8b2b95abc97e2752f44
Will McGugan <willmcgugan@gmail.com>
2023-06-19 18:28:57 +0100

    test

--- tests/test_call_later.py
+++ tests/test_call_later.py
@@ -1,6 +1,8 @@
 import asyncio
 
+from textual._context import active_message_pump
 from textual.app import App
+from textual.message_pump import MessagePump
 
 
 class CallLaterApp(App[None]):
@@ -30,13 +32,19 @@ async def test_call_after_refresh() -> None:
 
     called_event = asyncio.Event()
 
+    callback_message_pump: MessagePump | None = None
+
     def callback() -> None:
         nonlocal display_count
+        nonlocal callback_message_pump
         called_event.set()
         display_count = app.display_count
+        callback_message_pump = active_message_pump.get()
 
     async with app.run_test():
         app.call_after_refresh(callback)
         await asyncio.wait_for(called_event.wait(), 1)
         app_display_count = app.display_count
         assert app_display_count == display_count
+        # Should be app because the callback should be in the same context as app
+        assert callback_message_pump is app
