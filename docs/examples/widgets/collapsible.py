9e81a39cfc16997820369fa96f496242e0dccb9c
Sunyoung Yoo <luysunyoung@aifactory.page>
2023-07-22 16:46:00 +0200

    Add collapsible container example

---
+++ docs/examples/widgets/collapsible.py
@@ -0,0 +1,37 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Label, Markdown, Collapsible
+
+LETO = """
+# Duke Leto I Atreides
+
+Head of House Atreides.
+"""
+
+JESSICA = """
+# Lady Jessica
+
+Bene Gesserit and concubine of Leto, and mother of Paul and Alia.
+"""
+
+PAUL = """
+# Paul Atreides
+
+Son of Leto and Jessica.
+"""
+
+
+class CollapsibleApp(App):
+    """An example of colllapsible container."""
+
+    def compose(self) -> ComposeResult:
+        """Compose app with collapsible containers."""
+        with Collapsible(collapsed=False, summary="Leto"):
+            yield Label(LETO)
+        with Collapsible(collapsed=False, summary="Jessica"):
+            yield Markdown(JESSICA)
+        with Collapsible(collapsed=False, summary="Paul"):
+            yield Markdown(PAUL)
+
+if __name__ == "__main__":
+    app = CollapsibleApp()
+    app.run()
