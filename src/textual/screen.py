5ec78eef1d19b9c2349c4272c5b3d14a498dbe7a
Dave Pearson <davep@davep.org>
2023-07-23 21:00:37 +0100

    Remove the implementation of notify on Screen
    Screen inherits the one from Widget anyway. I always forget that Screen is a
    Widget. O_o

--- src/textual/screen.py
+++ src/textual/screen.py
@@ -940,30 +940,6 @@ class Screen(Generic[ScreenResultType], Widget):
         """
         self.dismiss(result)
 
-    def notify(
-        self,
-        message: str,
-        *,
-        title: str = "",
-        severity: SeverityLevel = "information",
-        timeout: float = Notification.timeout,
-    ) -> Notification:
-        """Create a notification.
-
-        Args:
-            message: The message for the notification.
-            title: The title for the notification.
-            severity: The severity of the notification.
-            timeout: The timeout for the notification.
-
-        Returns:
-            The new notification.
-
-        See [`App.notify`][textual.app.App.notify] for the full
-        documentation for this method.
-        """
-        return self.app.notify(message, title=title, severity=severity, timeout=timeout)
-
 
 @rich.repr.auto
 class ModalScreen(Screen[ScreenResultType]):
