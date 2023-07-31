93b5e71a7b34a8b62b722efa4d9989fd388bc347
Darren Burns <darrenb900@gmail.com>
2023-07-31 14:24:07 +0100

    Tests for selections in the Document

--- tests/document/test_document.py
+++ tests/document/test_document.py
@@ -50,3 +50,51 @@ def test_newline_unix():
 def test_newline_windows():
     document = Document(TEXT_WINDOWS)
     assert document.newline == "\r\n"
+
+
+def test_get_selected_text_no_selection():
+    document = Document(TEXT)
+    selection = document.get_selected_text((0, 0), (0, 0))
+    assert selection == ""
+
+
+def test_get_selected_text_single_line():
+    document = Document(TEXT_WINDOWS)
+    selection = document.get_selected_text((0, 2), (0, 6))
+    assert selection == "must"
+
+
+def test_get_selected_text_multiple_lines_unix():
+    document = Document(TEXT)
+    selection = document.get_selected_text((0, 2), (1, 2))
+    assert selection == "must not fear.\nFe"
+
+
+def test_get_selected_text_multiple_lines_windows():
+    document = Document(TEXT_WINDOWS)
+    selection = document.get_selected_text((0, 2), (1, 2))
+    assert selection == "must not fear.\r\nFe"
+
+
+def test_get_selected_text_including_final_newline_unix():
+    document = Document(TEXT_NEWLINE)
+    selection = document.get_selected_text((0, 0), (2, 0))
+    assert selection == TEXT_NEWLINE
+
+
+def test_get_selected_text_including_final_newline_windows():
+    document = Document(TEXT_WINDOWS_NEWLINE)
+    selection = document.get_selected_text((0, 0), (2, 0))
+    assert selection == TEXT_WINDOWS_NEWLINE
+
+
+def test_get_selected_text_no_newline_at_end_of_file():
+    document = Document(TEXT)
+    selection = document.get_selected_text((0, 0), (2, 0))
+    assert selection == TEXT
+
+
+def test_get_selected_text_no_newline_at_end_of_file_windows():
+    document = Document(TEXT_WINDOWS)
+    selection = document.get_selected_text((0, 0), (2, 0))
+    assert selection == TEXT_WINDOWS
