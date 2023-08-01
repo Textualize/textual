f540754f2eba1e2635309c071948d2c144d79571
Will McGugan <willmcgugan@gmail.com>
2023-08-01 18:12:02 +0100

    superfluous test

--- tests/test_log.py
+++
@@ -1,9 +0,0 @@
-from textual.widgets import Log
-
-
-def test_process_line():
-    assert Log._process_line("") == []
-    assert Log._process_line("foo") == ["foo"]
-    assert Log._process_line("foo\nbar") == ["foo", "bar"]
-    assert Log._process_line("\x00foo\nbar") == ["�foo", "bar"]
-    assert Log._process_line("\x00\tfoo\nbar") == ["�       foo", "bar"]
