d209d852af883171955a42690c323e3496f58f7f
Darren Burns <darrenb900@gmail.com>
2023-08-09 10:27:09 +0100

    Adding tests

--- tests/text_area/test_selection.py
+++ tests/text_area/test_selection.py
@@ -1,7 +1,6 @@
-import pytest
-
 from textual.app import App, ComposeResult
 from textual.document._document import Selection
+from textual.geometry import Offset
 from textual.widgets import TextArea
 
 TEXT = """I must not fear.
@@ -19,17 +18,13 @@ class TextAreaApp(App):
 
 
 def test_default_selection():
+    """The cursor starts at (0, 0) in the document."""
     text_area = TextArea()
     assert text_area.selection == Selection.cursor((0, 0))
 
 
-# def test_selection_modified():
-#     app = TextAreaApp()
-#     async with app.run_test():
-#         text_area = app.query_one(TextArea)
-
-
 async def test_selected_text_forward():
+    """Selecting text from top to bottom results in the correct selected_text."""
     app = TextAreaApp()
     async with app.run_test():
         text_area = app.query_one(TextArea)
@@ -44,6 +39,7 @@ Fear is the mind-killer.
 
 
 async def test_selected_text_backward():
+    """Selecting text from bottom to top results in the correct selected_text."""
     app = TextAreaApp()
     async with app.run_test():
         text_area = app.query_one(TextArea)
@@ -58,6 +54,7 @@ Fear is the mind-killer.
 
 
 async def test_selection_clamp():
+    """When you set the selection reactive, it's clamped to within the document bounds."""
     app = TextAreaApp()
     async with app.run_test():
         text_area = app.query_one(TextArea)
@@ -65,6 +62,72 @@ async def test_selection_clamp():
         assert text_area.selection == Selection(start=(4, 0), end=(4, 0))
 
 
-async def test_mouse_selection():
-    # TODO: Wednesday.
-    pass
+async def test_mouse_click():
+    """When you click the TextArea, the cursor moves to the expected location."""
+    app = TextAreaApp()
+    async with app.run_test() as pilot:
+        text_area = app.query_one(TextArea)
+        await pilot.click(TextArea, Offset(x=5, y=2))
+        assert text_area.selection == Selection.cursor((2, 2))
+
+
+async def test_mouse_click_clamp_from_right():
+    """When you click to the right of the document bounds, the cursor is clamped
+    to within the document bounds."""
+    app = TextAreaApp()
+    async with app.run_test() as pilot:
+        text_area = app.query_one(TextArea)
+        await pilot.click(TextArea, Offset(x=8, y=20))
+        assert text_area.selection == Selection.cursor((4, 0))
+
+
+async def test_mouse_click_gutter_clamp():
+    """When you click the gutter, it selects the start of the line."""
+    app = TextAreaApp()
+    async with app.run_test() as pilot:
+        text_area = app.query_one(TextArea)
+        await pilot.click(TextArea, Offset(x=0, y=3))
+        assert text_area.selection == Selection.cursor((3, 0))
+
+
+async def test_cursor_selection_right():
+    """When you press shift+right the selection is updated correctly."""
+    app = TextAreaApp()
+    async with app.run_test() as pilot:
+        text_area = app.query_one(TextArea)
+        await pilot.press(*["shift+right"] * 3)
+        assert text_area.selection == Selection((0, 0), (0, 3))
+
+
+async def test_cursor_selection_right():
+    """When you press shift+right resulting in the cursor moving to the next line,
+    the selection is updated correctly."""
+    app = TextAreaApp()
+    async with app.run_test() as pilot:
+        text_area = app.query_one(TextArea)
+        await pilot.press(*["shift+right"] * 3)
+        assert text_area.selection == Selection((0, 0), (0, 3))
+
+
+async def test_cursor_selection_left():
+    """When you press shift+left the selection is updated correctly."""
+    app = TextAreaApp()
+    async with app.run_test() as pilot:
+        text_area = app.query_one(TextArea)
+        text_area.selection = Selection.cursor((2, 5))
+        await pilot.press(*["shift+left"] * 3)
+        assert text_area.selection == Selection((2, 5), (2, 2))
+
+
+async def test_cursor_selection_left_to_previous_line():
+    """When you press shift+left resulting in the cursor moving back to the previous line,
+    the selection is updated correctly."""
+    app = TextAreaApp()
+    async with app.run_test() as pilot:
+        text_area = app.query_one(TextArea)
+        text_area.selection = Selection.cursor((2, 2))
+        await pilot.press(*["shift+left"] * 3)
+
+        # The cursor jumps up to the end of the line above.
+        end_of_previous_line = len(TEXT.splitlines()[1])
+        assert text_area.selection == Selection((2, 2), (1, end_of_previous_line))
