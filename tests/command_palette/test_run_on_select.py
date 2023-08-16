091ef4e868fd735c09de734f56f5d3632d190f0f
Dave Pearson <davep@davep.org>
2023-08-16 15:13:45 +0100

    Remove experimental change to tests

--- tests/command_palette/test_run_on_select.py
+++ tests/command_palette/test_run_on_select.py
@@ -33,7 +33,6 @@ class CommandPaletteRunOnSelectApp(App[None]):
 
 async def test_with_run_on_select_on() -> None:
     """With run on select on, the callable should be instantly run."""
-    save = CommandPalette.run_on_select
     async with CommandPaletteRunOnSelectApp().run_test() as pilot:
         assert isinstance(pilot.app, CommandPaletteRunOnSelectApp)
         pilot.app.action_command_palette()
@@ -41,7 +40,6 @@ async def test_with_run_on_select_on() -> None:
         await pilot.press("down")
         await pilot.press("enter")
         assert pilot.app.selection is not None
-    CommandPalette.run_on_select = save
 
 
 class CommandPaletteDoNotRunOnSelectApp(CommandPaletteRunOnSelectApp):
@@ -52,7 +50,6 @@ class CommandPaletteDoNotRunOnSelectApp(CommandPaletteRunOnSelectApp):
 
 async def test_with_run_on_select_off() -> None:
     """With run on select off, the callable should not be instantly run."""
-    save = CommandPalette.run_on_select
     async with CommandPaletteDoNotRunOnSelectApp().run_test() as pilot:
         assert isinstance(pilot.app, CommandPaletteDoNotRunOnSelectApp)
         pilot.app.action_command_palette()
@@ -63,4 +60,3 @@ async def test_with_run_on_select_off() -> None:
         assert pilot.app.query_one(Input).value != ""
         await pilot.press("enter")
         assert pilot.app.selection is not None
-    CommandPalette.run_on_select = save
