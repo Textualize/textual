9f33e65e0742f6729112bb74907a4df4af05e60f
Darren Burns <darrenb900@gmail.com>
2023-08-17 20:52:02 +0100

    Add docstring

--- src/textual/_fix_direction.py
+++ src/textual/_fix_direction.py
@@ -5,7 +5,15 @@ def _sort_ascending(
     start: tuple[int, int], end: tuple[int, int]
 ) -> tuple[tuple[int, int], tuple[int, int]]:
     """Given a range, return a new range (x, y) such
-    that x <= y which covers the same characters."""
+    that x <= y which covers the same characters.
+
+    Args:
+        start: The start of the range.
+        end: The end of the range.
+
+    Returns:
+        A tuple (lesser_point, greater_point).
+    """
     if start > end:
         return end, start
     return start, end
