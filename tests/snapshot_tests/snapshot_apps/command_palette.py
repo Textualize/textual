87100db150865d487796bbe095fe07122c3224b8
Dave Pearson <davep@davep.org>
2023-08-21 11:21:06 +0100

    Add a snapshot test for the command palette

---
+++ tests/snapshot_tests/snapshot_apps/command_palette.py
@@ -0,0 +1,24 @@
+from textual.app import App
+from textual.command_palette import CommandSource, CommandMatches, CommandSourceHit
+
+class TestSource(CommandSource):
+
+    def gndn(self) -> None:
+        pass
+
+    async def hunt_for(self, user_input: str) -> CommandMatches:
+        matcher = self.matcher.match(user_input)
+        for n in range(10):
+            command = f"This is a test of this code {n}"
+            yield CommandSourceHit(
+                n/10, matcher.highlight(command), self.gndn, command
+            )
+
+class CommandPaletteApp(App[None]):
+
+    def on_mount(self) -> None:
+        self.action_command_palette()
+
+if __name__ == "__main__":
+    CommandPaletteApp().run()
+
