c1d4dc8c4681db76da08caa27eca08c0c5d3cb4d
Will McGugan <willmcgugan@gmail.com>
2023-08-23 14:10:55 +0100

    debug

--- src/textual/drivers/web_driver.py
+++ src/textual/drivers/web_driver.py
@@ -161,7 +161,7 @@ class WebDriver(Driver):
             while not self.exit_event.is_set():
                 if wait_for_input(0.1):
                     data = read(fileno, 1024)  # raw data
-                    print(data)
+                    self.write_meta({"data": data})
                     for packet_type, payload in byte_stream.feed(data):
                         if packet_type == "D":
                             # Treat as stdin
