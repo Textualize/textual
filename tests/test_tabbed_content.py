4d0db5d13bb858545202e7c3a1342657de80c2ef
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-08-23 10:52:29 +0100

    Add regression test for #3145.

--- tests/test_tabbed_content.py
+++ tests/test_tabbed_content.py
@@ -696,3 +696,63 @@ async def test_showing_first_tab_activates_tab(tab_id: str):
         tabbed_content.show_tab(tab_id)
         await pilot.pause()
         assert tabbed_content.active == tab_id
+
+
+async def test_disabling_nested_tabs():
+    """Regression test for https://github.com/Textualize/textual/issues/3145."""
+
+    class TabbedApp(App):
+        def compose(self) -> ComposeResult:
+            with TabbedContent(id="tabbed-content"):
+                with TabPane("Tab Pane 1", id="first-tab"):
+                    yield Label("foo")
+                with TabPane("Tab Pane 2", id="second-tab"):
+                    yield Label("bar")
+                with TabPane("Tab Pane 3", id="third-tab"):
+                    with TabbedContent(id="another-tabbed-content"):
+                        with TabPane("Inner Pane 1", id="inner-tab-1"):
+                            yield Label("fizz")
+                        with TabPane("Inner Pane 2", id="inner-tab-2"):
+                            yield Label("bang")
+
+        def on_mount(self):
+            # tab_pane = self.query_one("#first-tab", expect_type=TabPane)  # WrongType error: unexpected type of ContentTab
+            # tab_pane = self.query_one("#first-tab")  # TooManyMatches error
+            tabber = self.query_one("#tabbed-content", expect_type=TabbedContent)
+            tabber.hide_tab(
+                "second-tab"
+            )  # TooManyMatches: Call to only_one resulted in more than one matched node
+
+    app = TabbedApp()
+    async with app.run_test() as pilot:
+        tabber = app.query_one("#tabbed-content", expect_type=TabbedContent)
+        tabber.disable_tab("second-tab")
+        await pilot.pause()
+        tabber.enable_tab("second-tab")
+        await pilot.pause()
+
+
+async def test_hiding_nested_tabs():
+    """Regression test for https://github.com/Textualize/textual/issues/3145."""
+
+    class TabbedApp(App):
+        def compose(self) -> ComposeResult:
+            with TabbedContent(id="tabbed-content"):
+                with TabPane("Tab Pane 1", id="first-tab"):
+                    yield Label("foo")
+                with TabPane("Tab Pane 2", id="second-tab"):
+                    yield Label("bar")
+                with TabPane("Tab Pane 3", id="third-tab"):
+                    with TabbedContent(id="another-tabbed-content"):
+                        with TabPane("Inner Pane 1", id="inner-tab-1"):
+                            yield Label("fizz")
+                        with TabPane("Inner Pane 2", id="inner-tab-2"):
+                            yield Label("bang")
+
+    app = TabbedApp()
+    async with app.run_test() as pilot:
+        tabber = app.query_one("#tabbed-content", expect_type=TabbedContent)
+        tabber.hide_tab("second-tab")
+        await pilot.pause()
+        tabber.show_tab("second-tab")
+        await pilot.pause()
