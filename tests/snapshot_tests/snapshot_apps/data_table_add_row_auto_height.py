12b2a0ab44744e9595c6fcd713bfa9d7b199cd66
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-09-11 17:44:48 +0100

    Improve data table tests.

--- tests/snapshot_tests/snapshot_apps/data_table_add_row_auto_height.py
+++ tests/snapshot_tests/snapshot_apps/data_table_add_row_auto_height.py
@@ -8,13 +8,17 @@ from textual.widgets import DataTable
 class AutoHeightRowsApp(App[None]):
     def compose(self):
         table = DataTable()
+        self.column = table.add_column("N")
         table.add_column("Column", width=10)
-        table.add_row("hey there", height=None)
-        table.add_row(Text("hey there"), height=None)
-        table.add_row(Text("long string", overflow="fold"), height=None)
-        table.add_row(Panel.fit("Hello\nworld"), height=None)
-        table.add_row("1\n2\n3\n4\n5\n6\n7", height=None)
+        table.add_row(3, "hey there", height=None)
+        table.add_row(1, Text("hey there"), height=None)
+        table.add_row(5, Text("long string", overflow="fold"), height=None)
+        table.add_row(2, Panel.fit("Hello\nworld"), height=None)
+        table.add_row(4, "1\n2\n3\n4\n5\n6\n7", height=None)
         yield table
+
+    def key_s(self):
+        self.query_one(DataTable).sort(self.column)
 
 
 if __name__ == "__main__":
