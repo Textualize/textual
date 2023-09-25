ee5b37f1bf4ed17167a578bee84b7de6df407470
Guy Avital <guyavital10@gmail.com>
2023-09-25 19:20:29 +0300

    Add countdown to showing command palette `no matches` message
    The countdown protects from spamming the message and helping the palette feel smoother when there are no matches

--- src/textual/command.py
+++ src/textual/command.py
@@ -458,6 +458,8 @@ class CommandPalette(ModalScreen[CallbackType], inherit_css=False):
         """The command that was selected by the user."""
         self._busy_timer: Timer | None = None
         """Keeps track of if there's a busy indication timer in effect."""
+        self._no_matches_timer: Timer | None = None
+        """Keeps track of if there are 'No matches found' message waiting to be displayed."""
         self._providers: list[Provider] = []
         """List of Provider instances involved in searches."""
 
@@ -558,6 +560,37 @@ class CommandPalette(ModalScreen[CallbackType], inherit_css=False):
                 self._show_busy = True
 
         self._busy_timer = self.set_timer(self._BUSY_COUNTDOWN, _become_busy)
+
+    def _stop_no_matches_countdown(self) -> None:
+        """Stop any 'No matches' countdown that's in effect."""
+        if self._no_matches_timer is not None:
+            self._no_matches_timer.stop()
+            self._no_matches_timer = None
+
+    _NO_MATCHES_COUNTDOWN: Final[float] = 0.5
+    """How many seconds to wait before showing 'No matches found'."""
+
+    def _start_no_matches_countdown(self) -> None:
+        """Start a countdown to showing that there are no matches for the query.
+
+        Adds a 'No matches found' option to the command list after `_NO_MATCHES_COUNTDOWN` seconds.
+        """
+        self._stop_no_matches_countdown()
+
+        def _show_no_matches() -> None:
+            command_list = self.query_one(CommandList)
+            command_list.add_option(
+                Option(
+                    Align.center(Text("No matches found")),
+                    disabled=True,
+                    id=self._NO_MATCHES,
+                )
+            )
+
+        self._no_matches_timer = self.set_timer(
+            self._NO_MATCHES_COUNTDOWN,
+            _show_no_matches,
+        )
 
     def _watch__list_visible(self) -> None:
         """React to the list visible flag being toggled."""
@@ -861,13 +894,7 @@ class CommandPalette(ModalScreen[CallbackType], inherit_css=False):
         # mean nothing was found. Give the user positive feedback to that
         # effect.
         if command_list.option_count == 0 and not worker.is_cancelled:
-            command_list.add_option(
-                Option(
-                    Align.center(Text("No matches found")),
-                    disabled=True,
-                    id=self._NO_MATCHES,
-                )
-            )
+            self._start_no_matches_countdown()
 
     @on(Input.Changed)
     def _input(self, event: Input.Changed) -> None:
@@ -878,6 +905,8 @@ class CommandPalette(ModalScreen[CallbackType], inherit_css=False):
         """
         event.stop()
         self.workers.cancel_all()
+        self._stop_no_matches_countdown()
+
         search_value = event.value.strip()
         if search_value:
             self._gather_commands(search_value)
