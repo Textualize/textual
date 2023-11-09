43d81a7a1be2cc8fa6c3334ed7e857b83bb4bcf5
Darren Burns <darrenb900@gmail.com>
2023-11-09 14:43:38 +0000

    Regression snapshot test for case where outline was applied to `Button` and did not render correctly.

---
+++ tests/snapshot_tests/snapshot_apps/button_outline.py
@@ -0,0 +1,17 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Button
+
+
+class ButtonIssue(App[None]):
+    CSS = """
+    Button {
+        outline: white;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Button("Test")
+
+
+if __name__ == "__main__":
+    ButtonIssue().run()
