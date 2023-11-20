aab538fb0c3341b156df6d386a2612f818d9723a
Darren Burns <darrenb900@gmail.com>
2023-11-20 17:12:16 +0000

    Remove irrelevant code

--- src/textual/widgets/_text_area.py
+++ src/textual/widgets/_text_area.py
@@ -27,7 +27,6 @@ from textual.document._syntax_aware_document import (
     SyntaxAwareDocument,
     SyntaxAwareDocumentError,
 )
-from textual.document._wrapped_document import WrappedDocument
 from textual.expand_tabs import expand_tabs_inline
 
 if TYPE_CHECKING:
@@ -213,9 +212,6 @@ TextArea {
     The text selected in the document is available via the `TextArea.selected_text` property.
     """
 
-    soft_wrap: Reactive[bool] = reactive(False)
-    """Enable or disable soft wrapping."""
-
     show_line_numbers: Reactive[bool] = reactive(True)
     """True to show the line number column on the left edge, otherwise False.
 
@@ -275,7 +271,6 @@ TextArea {
         *,
         language: str | None = None,
         theme: str | None = None,
-        soft_wrap: bool = False,
         name: str | None = None,
         id: str | None = None,
         classes: str | None = None,
@@ -340,12 +335,6 @@ TextArea {
         self.language = language
 
         self.theme = theme
-
-        self.soft_wrap = soft_wrap
-        """Enable or disable soft wrapping."""
-
-        self._wrapped_document = WrappedDocument()
-        """"""
 
     @staticmethod
     def _get_builtin_highlight_query(language_name: str) -> str:
@@ -660,10 +649,6 @@ TextArea {
             document = Document(text)
 
         self.document = document
-
-        self._wrapped_document = WrappedDocument(document)
-        self._wrapped_document.wrap_all()
-
         self._build_highlight_map()
 
     @property
