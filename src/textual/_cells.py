e2ff0c4c433c3648696316fc3f1e0b314e056b59
Darren Burns <darrenb900@gmail.com>
2023-11-22 15:41:44 +0000

    Fix click target issue when tabs not fully expanded

--- src/textual/_cells.py
+++ src/textual/_cells.py
@@ -1,6 +1,6 @@
 from typing import Callable
 
-from textual.expand_tabs import expand_tabs_inline
+from textual.expand_tabs import get_tab_widths
 
 __all__ = ["cell_len", "cell_width_to_column_index"]
 
@@ -23,9 +23,22 @@ def cell_width_to_column_index(line: str, cell_width: int, tab_width: int) -> in
     Returns:
         The column corresponding to the cell width.
     """
+    column_index = 0
     total_cell_offset = 0
-    for column_index, character in enumerate(line):
-        total_cell_offset += cell_len(expand_tabs_inline(character, tab_width))
-        if total_cell_offset >= cell_width + 1:
+    for part, expanded_tab_width in get_tab_widths(line, tab_width):
+        # Check if the click landed on a character within this part.
+        for character in part:
+            total_cell_offset += cell_len(character)
+            if total_cell_offset > cell_width:
+                return column_index
+            column_index += 1
+
+        # Account for the appearance of the tab character for this part
+        total_cell_offset += expanded_tab_width
+        # Check if the click falls within the boundary of the expanded tab.
+        if total_cell_offset > cell_width:
             return column_index
+
+        column_index += 1
+
     return len(line)
--- src/textual/expand_tabs.py
+++ src/textual/expand_tabs.py
@@ -7,6 +7,41 @@ from rich.cells import cell_len
 _TABS_SPLITTER_RE = re.compile(r"(.*?\t|.+?$)")
 
 
+def get_tab_widths(line: str, tab_size: int = 4) -> list[tuple[str, int]]:
+    """Splits a string line into tuples (str, int).
+
+    Args:
+        line: The text to expand tabs in.
+        tab_size: Number of cells in a tab.
+
+    Returns:
+        Each tuple represents a section of the line which precedes a tab character.
+            The string is the string text that appears before the tab character (excluding the tab).
+            The integer is the width that the tab character is expanded to.
+    """
+
+    parts: list[tuple[str, int]] = []
+    add_part = parts.append
+    cell_position = 0
+    matches = _TABS_SPLITTER_RE.findall(line)
+
+    for match in matches:
+        expansion_width = 0
+        if match.endswith("\t"):
+            # Remove the tab, and check the width of the rest of the line.
+            match = match[:-1]
+            cell_position += cell_len(match)
+
+            # Now move along the line by the width of the tab.
+            tab_remainder = cell_position % tab_size
+            expansion_width = tab_size - tab_remainder
+            cell_position += expansion_width
+
+        add_part((match, expansion_width))
+
+    return parts
+
+
 def expand_tabs_inline(line: str, tab_size: int = 4) -> str:
     """Expands tabs, taking into account double cell characters.
 
@@ -16,28 +51,15 @@ def expand_tabs_inline(line: str, tab_size: int = 4) -> str:
     Returns:
         New string with tabs replaced with spaces.
     """
-    if "\t" not in line:
-        return line
-    new_line_parts: list[str] = []
-    add_part = new_line_parts.append
-    cell_position = 0
-    parts = _TABS_SPLITTER_RE.findall(line)
-
-    for part in parts:
-        if part.endswith("\t"):
-            part = f"{part[:-1]} "
-            cell_position += cell_len(part)
-            tab_remainder = cell_position % tab_size
-            if tab_remainder:
-                spaces = tab_size - tab_remainder
-                part += spaces * " "
-        add_part(part)
-
-    return "".join(new_line_parts)
+    tab_widths = get_tab_widths(line, tab_size)
+    return "".join(
+        [part + expansion_width * " " for part, expansion_width in tab_widths]
+    )
 
 
 if __name__ == "__main__":
     print(expand_tabs_inline("\tbar"))
+    print(expand_tabs_inline("\tbar\t"))
     print(expand_tabs_inline("1\tbar"))
     print(expand_tabs_inline("12\tbar"))
     print(expand_tabs_inline("123\tbar"))
