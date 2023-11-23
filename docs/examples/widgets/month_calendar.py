726274b26c4d855796490a6f150d64402e766e0b
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-11-23 20:56:10 +0000

    add bindings to example

--- docs/examples/widgets/month_calendar.py
+++ docs/examples/widgets/month_calendar.py
@@ -1,10 +1,30 @@
 from textual.app import App, ComposeResult
+from textual.binding import Binding
 from textual.widgets import MonthCalendar
 
 
 class MonthCalendarApp(App):
+    BINDINGS = [
+        Binding("pageup", "next_month", priority=True),
+        Binding("pagedown", "previous_month", priority=True),
+        Binding("ctrl+pageup", "next_year", priority=True),
+        Binding("ctrl+pagedown", "previous_year", priority=True),
+    ]
+
     def compose(self) -> ComposeResult:
         yield MonthCalendar(year=2021, month=6)
+
+    def action_next_month(self) -> None:
+        self.query_one(MonthCalendar).next_month()
+
+    def action_previous_month(self) -> None:
+        self.query_one(MonthCalendar).previous_month()
+
+    def action_next_year(self) -> None:
+        self.query_one(MonthCalendar).next_year()
+
+    def action_previous_year(self) -> None:
+        self.query_one(MonthCalendar).previous_year()
 
 
 if __name__ == "__main__":
