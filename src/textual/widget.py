b8de32440f9f64d2054c0fba439ee856b179741c
Will McGugan <willmcgugan@gmail.com>
2023-12-05 18:14:18 +0000

    ws removal

--- src/textual/widget.py
+++ src/textual/widget.py
@@ -257,7 +257,7 @@ class Widget(DOMNode):
         link-background-hover: $accent;
         link-color-hover: $text;
         link-style-hover: bold not underline;
-    }    
+    }
     """
     COMPONENT_CLASSES: ClassVar[set[str]] = set()
 
