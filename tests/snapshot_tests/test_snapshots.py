f07c1ee50f9ed634ea13735131a0673177130e6b
Will McGugan <willmcgugan@gmail.com>
2023-12-17 09:36:07 +0000

    increase wait

--- tests/snapshot_tests/test_snapshots.py
+++ tests/snapshot_tests/test_snapshots.py
@@ -681,7 +681,7 @@ def test_scroll_to_center(snap_compare):
 def test_quickly_change_tabs(snap_compare):
     # https://github.com/Textualize/textual/issues/2229
     assert snap_compare(
-        SNAPSHOT_APPS_DIR / "quickly_change_tabs.py", press=["p", "wait:50"]
+        SNAPSHOT_APPS_DIR / "quickly_change_tabs.py", press=["p", "wait:350"]
     )
 
 
