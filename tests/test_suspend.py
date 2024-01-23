faf9b51a51caad9ea931114db385fdbe45685918
Dave Pearson <davep@davep.org>
2024-01-23 11:49:40 +0000

    Add a test for doing a suspend
    Borrowing heavily from Josh's testing.
    
    Co-authored-by: Josh Karpel <josh.karpel@gmail.com>

--- tests/test_suspend.py
+++ tests/test_suspend.py
@@ -1,6 +1,9 @@
+import sys
+
 import pytest
 
 from textual.app import App, SuspendNotSupported
+from textual.drivers.headless_driver import HeadlessDriver
 
 
 async def test_suspend_not_supported() -> None:
@@ -11,3 +14,41 @@ async def test_suspend_not_supported() -> None:
         with pytest.raises(SuspendNotSupported):
             with pilot.app.suspend():
                 pass
+
+
+async def test_suspend_supported(capfd: pytest.CaptureFixture[str]) -> None:
+    """Suspending when supported should call the relevant driver methods."""
+
+    calls: set[str] = set()
+
+    class HeadlessSuspendDriver(HeadlessDriver):
+        @property
+        def is_headless(self) -> bool:
+            return False
+
+        @property
+        def can_suspend(self) -> bool:
+            return True
+
+        def start_application_mode(self) -> None:
+            nonlocal calls
+            calls.add("start")
+
+        def stop_application_mode(self) -> None:
+            nonlocal calls
+            calls.add("stop")
+
+        def close(self) -> None:
+            nonlocal calls
+            calls.add("close")
+
+    async with App(driver_class=HeadlessSuspendDriver).run_test(
+        headless=False
+    ) as pilot:
+        calls = set()
+        with pilot.app.suspend():
+            _ = capfd.readouterr()  # Clear the existing buffer.
+            print("USE THEM TOGETHER.", end="", flush=True)
+            print("USE THEM IN PEACE.", file=sys.stderr, end="", flush=True)
+            assert ("USE THEM TOGETHER.", "USE THEM IN PEACE.") == capfd.readouterr()
+        assert calls == {"start", "stop", "close"}
