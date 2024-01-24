f1bbb6a73d565255daf6bcdaeaf6bd14baf1f612
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2024-01-24 22:03:09 +0000

    fix(app): change render return type to RenderResult

--- docs/examples/how-to/render_compose.py
+++ docs/examples/how-to/render_compose.py
@@ -1,5 +1,7 @@
 from time import time
 
+from rich.console import RenderableType
+
 from textual.app import App, ComposeResult, RenderableType
 from textual.containers import Container
 from textual.renderables.gradient import LinearGradient
--- src/textual/app.py
+++ src/textual/app.py
@@ -1497,7 +1497,7 @@ class App(Generic[ReturnType], DOMNode):
                 for screen in self.screen_stack:
                     self.stylesheet.update(screen)
 
-    def render(self) -> RenderableType:
+    def render(self) -> RenderResult:
         return Blank(self.styles.background)
 
     ExpectType = TypeVar("ExpectType", bound=Widget)
