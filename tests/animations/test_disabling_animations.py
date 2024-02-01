15b2063e2ad650285972ebb85e7637b66fc681b8
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2024-02-01 10:27:44 +0000

    Remove test flakiness.

--- tests/animations/test_disabling_animations.py
+++ tests/animations/test_disabling_animations.py
@@ -24,20 +24,21 @@ async def test_style_animations_via_animate_work_on_full() -> None:
     app = SingleLabelApp()
     app.animation_level = "full"
 
-    async with app.run_test() as pilot:
+    async with app.run_test():
         label = app.query_one(Label)
         # Sanity check.
         assert label.styles.background == Color.parse("red")
-        # Test.
         animator = app.animator
         # Freeze time at 0 before triggering the animation.
         animator._get_time = lambda *_: 0
         label.styles.animate("background", "blue", duration=1)
-        await pilot.pause()
-        # Freeze time after the animation start and before animation end.
-        animator._get_time = lambda *_: 0.01
+        assert len(animator._animations) > 0  # Sanity check.
+        # Freeze time around the animation midpoint.
+        animator._get_time = lambda *_: 0.5
         # Move to the next frame.
         await animator()
+        # The animation shouldn't have completed.
+        assert label.styles.background != Color.parse("red")
         assert label.styles.background != Color.parse("blue")
 
 
@@ -45,20 +46,20 @@ async def test_style_animations_via_animate_are_disabled_on_basic() -> None:
     app = SingleLabelApp()
     app.animation_level = "basic"
 
-    async with app.run_test() as pilot:
+    async with app.run_test():
         label = app.query_one(Label)
         # Sanity check.
         assert label.styles.background == Color.parse("red")
-        # Test.
         animator = app.animator
         # Freeze time at 0 before triggering the animation.
         animator._get_time = lambda *_: 0
         label.styles.animate("background", "blue", duration=1)
-        await pilot.pause()
+        assert len(animator._animations) > 0  # Sanity check.
         # Freeze time after the animation start and before animation end.
         animator._get_time = lambda *_: 0.01
         # Move to the next frame.
         await animator()
+        # The animation should have completed.
         assert label.styles.background == Color.parse("blue")
 
 
@@ -66,20 +67,20 @@ async def test_style_animations_via_animate_are_disabled_on_none() -> None:
     app = SingleLabelApp()
     app.animation_level = "none"
 
-    async with app.run_test() as pilot:
+    async with app.run_test():
         label = app.query_one(Label)
         # Sanity check.
         assert label.styles.background == Color.parse("red")
-        # Test.
         animator = app.animator
         # Freeze time at 0 before triggering the animation.
         animator._get_time = lambda *_: 0
         label.styles.animate("background", "blue", duration=1)
-        await pilot.pause()
+        assert len(animator._animations) > 0  # Sanity check.
         # Freeze time after the animation start and before animation end.
         animator._get_time = lambda *_: 0.01
         # Move to the next frame.
         await animator()
+        # The animation should have completed.
         assert label.styles.background == Color.parse("blue")
 
 
@@ -105,13 +106,20 @@ async def test_style_animations_via_transition_work_on_full() -> None:
     app = LabelWithTransitionsApp()
     app.animation_level = "full"
 
-    async with app.run_test() as pilot:
+    async with app.run_test():
         label = app.query_one(Label)
         # Sanity check.
         assert label.styles.background == Color.parse("red")
-        # Test.
+        animator = app.animator
+        # Free time at 0 before triggering the animation.
+        animator._get_time = lambda *_: 0
         label.add_class("blue-bg")
-        await pilot.pause()
+        assert len(animator._animations) > 0  # Sanity check.
+        # Freeze time in the middle of the animation.
+        animator._get_time = lambda *_: 0.5
+        await animator()
+        # The animation should be undergoing.
+        assert label.styles.background != Color.parse("red")
         assert label.styles.background != Color.parse("blue")
 
 
@@ -119,13 +127,19 @@ async def test_style_animations_via_transition_are_disabled_on_basic() -> None:
     app = LabelWithTransitionsApp()
     app.animation_level = "basic"
 
-    async with app.run_test() as pilot:
+    async with app.run_test():
         label = app.query_one(Label)
         # Sanity check.
         assert label.styles.background == Color.parse("red")
-        # Test.
+        animator = app.animator
+        # Free time at 0 before triggering the animation.
+        animator._get_time = lambda *_: 0
         label.add_class("blue-bg")
-        await pilot.pause()
+        assert len(animator._animations) > 0  # Sanity check.
+        # Freeze time after the animation start and before animation end.
+        animator._get_time = lambda *_: 0.01
+        await animator()
+        # The animation should have completed.
         assert label.styles.background == Color.parse("blue")
 
 
@@ -133,11 +147,17 @@ async def test_style_animations_via_transition_are_disabled_on_none() -> None:
     app = LabelWithTransitionsApp()
     app.animation_level = "none"
 
-    async with app.run_test() as pilot:
+    async with app.run_test():
         label = app.query_one(Label)
         # Sanity check.
         assert label.styles.background == Color.parse("red")
-        # Test.
+        animator = app.animator
+        # Free time at 0 before triggering the animation.
+        animator._get_time = lambda *_: 0
         label.add_class("blue-bg")
-        await pilot.pause()
+        assert len(animator._animations) > 0  # Sanity check.
+        # Freeze time after the animation start and before animation end.
+        animator._get_time = lambda *_: 0.01
+        await animator()
+        # The animation should have completed.
         assert label.styles.background == Color.parse("blue")
