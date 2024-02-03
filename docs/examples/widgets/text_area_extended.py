ccb78546e50891aa77fa8bf9f6d593b812776c3f
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2024-02-03 02:01:17 +0000

    revert text_area_extended.py

--- docs/examples/widgets/text_area_extended.py
+++ docs/examples/widgets/text_area_extended.py
@@ -6,29 +6,6 @@ from textual.widgets import TextArea
 class ExtendedTextArea(TextArea):
     """A subclass of TextArea with parenthesis-closing functionality."""
 
-    def __init__(
-        self,
-        text: str = "",
-        *,
-        language: str | None = None,
-        name: str | None = None,
-        id: str | None = None,
-        classes: str | None = None,
-        disabled: bool = False
-    ) -> None:
-        super().__init__(
-            text=text,
-            language=language,
-            theme="monokai",
-            soft_wrap=False,
-            tab_behaviour="indent",
-            show_line_numbers=True,
-            name=name,
-            id=id,
-            classes=classes,
-            disabled=disabled,
-        )
-
     def _on_key(self, event: events.Key) -> None:
         if event.character == "(":
             self.insert("()")
