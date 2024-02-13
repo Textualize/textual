e59fd0e2a20892afca210ad3ab6a7b13fbbc3147
Dave Pearson <davep@davep.org>
2024-02-13 12:43:24 +0000

    Deduplicate the collection of system commands in the provider

--- src/textual/_system_commands.py
+++ src/textual/_system_commands.py
@@ -4,7 +4,10 @@ This is a simple command provider that makes the most obvious application
 actions available via the [command palette][textual.command.CommandPalette].
 """
 
+from __future__ import annotations
+
 from .command import DiscoveryHit, Hit, Hits, Provider
+from .types import IgnoreReturnCallbackType
 
 
 class SystemCommands(Provider):
@@ -12,6 +15,40 @@ class SystemCommands(Provider):
 
     Used by default in [`App.COMMANDS`][textual.app.App.COMMANDS].
     """
+
+    @property
+    def _system_commands(self) -> tuple[tuple[str, IgnoreReturnCallbackType, str], ...]:
+        """The system commands to reveal to the command palette."""
+        return (
+            (
+                "Toggle light/dark mode",
+                self.app.action_toggle_dark,
+                "Toggle the application between light and dark mode",
+            ),
+            (
+                "Quit the application",
+                self.app.action_quit,
+                "Quit the application as soon as possible",
+            ),
+            (
+                "Ring the bell",
+                self.app.action_bell,
+                "Ring the terminal's 'bell'",
+            ),
+        )
+
+    async def discover(self) -> Hits:
+        """Handle a request for the discovery commands for this provider.
+
+        Yields:
+            Commands that can be discovered.
+        """
+        for name, runnable, help_text in self._system_commands:
+            yield DiscoveryHit(
+                name,
+                runnable,
+                help=help_text,
+            )
 
     async def search(self, query: str) -> Hits:
         """Handle a request to search for system commands that match the query.
@@ -28,23 +65,7 @@ class SystemCommands(Provider):
 
         # Loop over all applicable commands, find those that match and offer
         # them up to the command palette.
-        for name, runnable, help_text in (
-            (
-                "Toggle light/dark mode",
-                self.app.action_toggle_dark,
-                "Toggle the application between light and dark mode",
-            ),
-            (
-                "Quit the application",
-                self.app.action_quit,
-                "Quit the application as soon as possible",
-            ),
-            (
-                "Ring the bell",
-                self.app.action_bell,
-                "Ring the terminal's 'bell'",
-            ),
-        ):
+        for name, runnable, help_text in self._system_commands:
             match = matcher.match(name)
             if match > 0:
                 yield Hit(
@@ -53,33 +74,3 @@ class SystemCommands(Provider):
                     runnable,
                     help=help_text,
                 )
-
-    async def discover(self) -> Hits:
-        """Handle a request for the discovery commands for this provider.
-
-        Yields:
-            Commands that can be discovered.
-        """
-        # TODO: Dedupe these from the above.
-        for name, runnable, help_text in (
-            (
-                "Toggle light/dark mode",
-                self.app.action_toggle_dark,
-                "Toggle the application between light and dark mode",
-            ),
-            (
-                "Quit the application",
-                self.app.action_quit,
-                "Quit the application as soon as possible",
-            ),
-            (
-                "Ring the bell",
-                self.app.action_bell,
-                "Ring the terminal's 'bell'",
-            ),
-        ):
-            yield DiscoveryHit(
-                name,
-                runnable,
-                help=help_text,
-            )
