f45df6a0b4ae77ac116daea571ef7579f44f38f0
Dave Pearson <davep@davep.org>
2024-03-04 10:16:46 +0000

    Add a test for getting a message when changing collapsible via reactive
    This is currently a failing test; work relating to #3869 should make this
    pass.

--- tests/test_collapsible.py
+++ tests/test_collapsible.py
@@ -213,6 +213,28 @@ async def test_collapse_message():
         assert len(hits) == 1
 
 
+async def test_collapse_via_watcher_message():
+    """Setting `collapsed` to `True` should post a message."""
+
+    hits = []
+
+    class CollapsibleApp(App[None]):
+        def compose(self) -> ComposeResult:
+            yield Collapsible(collapsed=False)
+
+        def on_collapsible_collapsed(self) -> None:
+            hits.append("collapsed")
+
+    async with CollapsibleApp().run_test() as pilot:
+        assert not pilot.app.query_one(Collapsible).collapsed
+
+        pilot.app.query_one(Collapsible).collapsed = True
+        await pilot.pause()
+
+        assert pilot.app.query_one(Collapsible).collapsed
+        assert len(hits) == 1
+
+
 async def test_collapsible_title_reactive_change():
     class CollapsibleApp(App[None]):
         def compose(self) -> ComposeResult:
