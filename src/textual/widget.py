7122baa036c23d7b399719b60986df32fb79c818
Will McGugan <willmcgugan@gmail.com>
2024-03-16 15:14:19 +0000

    just typing

--- src/textual/widget.py
+++ src/textual/widget.py
@@ -2937,10 +2937,10 @@ class Widget(DOMNode):
         scrollbar_size_horizontal = styles.scrollbar_size_horizontal
         scrollbar_size_vertical = styles.scrollbar_size_vertical
 
-        show_vertical_scrollbar: bool = bool(
+        show_vertical_scrollbar = bool(
             show_vertical_scrollbar and scrollbar_size_vertical
         )
-        show_horizontal_scrollbar: bool = bool(
+        show_horizontal_scrollbar = bool(
             show_horizontal_scrollbar and scrollbar_size_horizontal
         )
 
@@ -2974,10 +2974,10 @@ class Widget(DOMNode):
         scrollbar_size_horizontal = self.scrollbar_size_horizontal
         scrollbar_size_vertical = self.scrollbar_size_vertical
 
-        show_vertical_scrollbar: bool = bool(
+        show_vertical_scrollbar = bool(
             show_vertical_scrollbar and scrollbar_size_vertical
         )
-        show_horizontal_scrollbar: bool = bool(
+        show_horizontal_scrollbar = bool(
             show_horizontal_scrollbar and scrollbar_size_horizontal
         )
 
