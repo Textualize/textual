2e01ab3553090e17c446b28af9ab225ddcb20ce0
Will McGugan <willmcgugan@gmail.com>
2024-05-16 21:15:48 +0100

    test fixes

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -8,20 +8,24 @@ from textual.actions import ActionError, parse
 
 
 @pytest.mark.parametrize(
-    ("action_string", "expected_name", "expected_arguments"),
+    ("action_string", "expected_namespace", "expected_name", "expected_arguments"),
     [
-        ("spam", "spam", ()),
-        ("hypothetical_action()", "hypothetical_action", ()),
-        ("another_action(1)", "another_action", (1,)),
-        ("foo(True, False)", "foo", (True, False)),
-        ("foo.bar.baz(3, 3.15, 'Python')", "foo.bar.baz", (3, 3.15, "Python")),
-        ("m1234.n5678(None, [1, 2])", "m1234.n5678", (None, [1, 2])),
+        ("spam", "", "spam", ()),
+        ("hypothetical_action()", "", "hypothetical_action", ()),
+        ("another_action(1)", "", "another_action", (1,)),
+        ("foo(True, False)", "", "foo", (True, False)),
+        ("foo.bar.baz(3, 3.15, 'Python')", "foo.bar", "baz", (3, 3.15, "Python")),
+        ("m1234.n5678(None, [1, 2])", "m1234", "n5678", (None, [1, 2])),
     ],
 )
 def test_parse_action(
-    action_string: str, expected_name: str, expected_arguments: tuple[Any]
+    action_string: str,
+    expected_namespace: str,
+    expected_name: str,
+    expected_arguments: tuple[Any],
 ) -> None:
-    action_name, action_arguments = parse(action_string)
+    namespace, action_name, action_arguments = parse(action_string)
+    assert namespace == expected_namespace
     assert action_name == expected_name
     assert action_arguments == expected_arguments
 
@@ -44,7 +48,7 @@ def test_nested_and_convoluted_tuple_arguments(
     action_string: str, expected_arguments: tuple[Any]
 ) -> None:
     """Test that tuple arguments are parsed correctly."""
-    _, args = parse(action_string)
+    _, _, args = parse(action_string)
     assert args == expected_arguments
 
 
@@ -67,7 +71,7 @@ def test_parse_action_nested_special_character_arguments(
 
     See also: https://github.com/Textualize/textual/issues/2088
     """
-    _, args = parse(action_string)
+    _, _, args = parse(action_string)
     assert args == expected_arguments
 
 
