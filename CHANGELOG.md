2ffe752d348040fadbbcf6fa5ca21d9dc6770593
Travis Wrightsman <travis@wrightsman.org>
2024-05-17 17:28:14 -0400

    Return correct bindings inside modal screens

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## Unreleased
+
+### Fixed
+
+- Return correct bindings inside modal screens
+
 ## [0.60.1] - 2024-05-15
 
 ### Fixed
--- src/textual/app.py
+++ src/textual/app.py
@@ -868,7 +868,7 @@ class App(Generic[ReturnType], DOMNode):
         """
 
         bindings_map: dict[str, tuple[DOMNode, Binding]] = {}
-        for namespace, bindings in self._binding_chain:
+        for namespace, bindings in self._modal_binding_chain:
             for key, binding in bindings.keys.items():
                 if existing_key_and_binding := bindings_map.get(key):
                     _, existing_binding = existing_key_and_binding
