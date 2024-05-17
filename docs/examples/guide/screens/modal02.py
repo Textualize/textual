4378f779af6ed7d664c428ccb95c45922c4844fc
Will McGugan <willmcgugan@gmail.com>
2024-05-17 19:30:03 +0100

    restore modal

--- docs/examples/guide/screens/modal02.py
+++ docs/examples/guide/screens/modal02.py
@@ -15,8 +15,6 @@ Where the fear has gone there will be nothing. Only I will remain."""
 class QuitScreen(ModalScreen):
     """Screen with a dialog to quit."""
 
-    BINDINGS = [("f", "request_foo", "Foo")]
-
     def compose(self) -> ComposeResult:
         yield Grid(
             Label("Are you sure you want to quit?", id="question"),
@@ -24,16 +22,12 @@ class QuitScreen(ModalScreen):
             Button("Cancel", variant="primary", id="cancel"),
             id="dialog",
         )
-        yield Footer()
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button.id == "quit":
             self.app.exit()
         else:
             self.app.pop_screen()
-
-    def action_request_foo(self) -> None:
-        print("!!")
 
 
 class ModalApp(App):
