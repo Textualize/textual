3219d1578b184f319048cf745a4210d54aa2d51c
Will McGugan <willmcgugan@gmail.com>
2024-05-19 20:18:34 +0100

    docstrings

--- examples/markdown.py
+++ examples/markdown.py
@@ -27,31 +27,39 @@ class MarkdownApp(App):
         yield MarkdownViewer()
 
     async def on_mount(self) -> None:
-        self.markdown_viewer.focus()
+        """Go to the first path when the app starts."""
         try:
             await self.markdown_viewer.go(self.path)
         except FileNotFoundError:
             self.exit(message=f"Unable to load {self.path!r}")
 
     def on_markdown_viewer_navigator_updated(self) -> None:
+        """Refresh bindings for forward / back when the document changes."""
         self.refresh_bindings()
 
     def action_toggle_table_of_contents(self) -> None:
+        """Toggles display of the table of contents."""
         self.markdown_viewer.show_table_of_contents = (
             not self.markdown_viewer.show_table_of_contents
         )
 
     async def action_back(self) -> None:
+        """Navigate backwards."""
         await self.markdown_viewer.back()
 
     async def action_forward(self) -> None:
+        """Navigate forwards."""
         await self.markdown_viewer.forward()
 
-    def check_action(self, action: str, parameters: tuple[object, ...]) -> bool | None:
+    def check_action(self, action: str, _) -> bool | None:
+        """Check if certain actions can be performed."""
         if action == "forward" and self.markdown_viewer.navigator.end:
+            # Disable footer link if we can't go forward
             return None
         if action == "back" and self.markdown_viewer.navigator.start:
+            # Disable footer link if we can't go backward
             return None
+        # All other keys display as normal
         return True
 
 
