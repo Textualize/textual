bc067adbf3d4ceedcb9aa692e5027ada1b0c258a
Will McGugan <willmcgugan@gmail.com>
2024-05-22 17:48:03 +0100

    Deployed 915b7691f with MkDocs version: 1.5.3

---
+++ examples/widgets/classic_footer.py
@@ -0,0 +1,25 @@
+from textual.app import App, ComposeResult
+from textual.binding import Binding
+from textual.widgets import ClassicFooter
+
+
+class FooterApp(App):
+    BINDINGS = [
+        Binding(key="q", action="quit", description="Quit the app"),
+        Binding(
+            key="question_mark",
+            action="help",
+            description="Show help screen",
+            key_display="?",
+        ),
+        Binding(key="delete", action="delete", description="Delete the thing"),
+        Binding(key="j", action="down", description="Scroll down", show=False),
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield ClassicFooter()
+
+
+if __name__ == "__main__":
+    app = FooterApp()
+    app.run()
