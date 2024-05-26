dee6b4ad0955368b544da5d40af6e9db377b4fdd
Will McGugan <willmcgugan@gmail.com>
2024-05-26 14:06:11 +0100

    remove faulty test

--- tests/test_freeze.py
+++ tests/test_freeze.py
@@ -1,10 +1,8 @@
 import pytest
 
-from textual import on
-from textual.app import App, ComposeResult
-from textual.containers import ScrollableContainer
-from textual.screen import ModalScreen, Screen
-from textual.widgets import Button, Footer, Header, Input, Label
+from textual.app import App
+from textual.screen import Screen
+from textual.widgets import Footer, Header, Input
 
 
 class MyScreen(Screen):
@@ -26,39 +24,3 @@ async def test_freeze():
     with pytest.raises(Exception):
         async with app.run_test():
             raise Exception("never raised")
-
-
-async def test_button_freeze():
-    class MyModal(ModalScreen[str | None]):
-        BINDINGS = [("escape", "cancel", "Cancel")]
-
-        def __init__(self, s: str):
-            self._s = s
-            super().__init__()
-
-        def compose(self) -> ComposeResult:
-            with ScrollableContainer():
-                yield Label("My Screen")
-                yield Button("Save", id="save", variant="success")
-            yield Footer()
-
-        @on(Button.Pressed, "#save")
-        def handle_save(self) -> None:
-            self.dismiss("Yes" if (self._s == "Hello!") else None)
-
-        def action_cancel(self) -> None:
-            self.dismiss(None)
-
-    class MyApp(App):
-        BINDINGS = [("m", "modal", "Show modal")]
-
-        def compose(self) -> ComposeResult:
-            yield Footer()
-
-        def action_modal(self) -> None:
-            s = "Hello!"
-            self.push_screen(MyModal(s))
-
-    app = MyApp()
-    async with app.run_test() as pilot:
-        await pilot.press("tab", "return")
