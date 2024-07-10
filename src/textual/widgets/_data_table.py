b5e165f533641ccf75a7bb732e161328f8e9eece
Charles Merriam <charles.merriam@gmail.com>
2024-07-10 08:38:05 -0700

    Improvements to Widget DataTable documentation (#4706)
    * Preamble.  Mention commonly searched for capabilities missing, e.g., formatting and filtering.
    
    * Clarify zebra_stripes as using alternating styles.
    
    * Expand Cursors section to mention None type of cursor, clarify keyboard and mouse events, and that row indices start at one.  Expand example for No Cursor option.
    
    Also minor typos to previous two commits (preamble wording and zebra stripes).
    
    * Expand sorting section for clarity.
    
    Minor typos in Labeled Rows and Cursors sections.
    
    * In Keys section, mention functions taking coordinates.
    
    Fix typo in Sorting section.
    
    * Minor changes
    
    ---------
    
    Co-authored-by: Darren Burns <darrenb900@gmail.com>

--- src/textual/widgets/_data_table.py
+++ src/textual/widgets/_data_table.py
@@ -264,8 +264,8 @@ class DataTable(ScrollView, Generic[CellType], can_focus=True):
     | `datatable--header` | Target the header of the data table. |
     | `datatable--header-cursor` | Target cells highlighted by the cursor. |
     | `datatable--header-hover` | Target hovered header or row label cells. |
-    | `datatable--even-row` | Target even rows (row indices start at 0). |
-    | `datatable--odd-row` | Target odd rows (row indices start at 0). |
+    | `datatable--even-row` | Target even rows (row indices start at 0) if zebra_stripes. |
+    | `datatable--odd-row` | Target odd rows (row indices start at 0) if zebra_stripes. |
     """
 
     DEFAULT_CSS = """
@@ -720,7 +720,8 @@ class DataTable(ScrollView, Generic[CellType], can_focus=True):
         self.fixed_columns = fixed_columns
         """The number of columns to fix (prevented from scrolling)."""
         self.zebra_stripes = zebra_stripes
-        """Apply zebra effect on row backgrounds (light, dark, light, dark, ...)."""
+        """Apply alternating styles, datatable--even-row and datatable-odd-row, to create a zebra effect, e.g., 
+        alternating light and dark backgrounds."""
         self.show_cursor = show_cursor
         """Show/hide both the keyboard and hover cursor."""
         self.cursor_foreground_priority = cursor_foreground_priority
