03d3616d845044bb69b336fcda4666ae69b5a0af
Darren Burns <darrenb900@gmail.com>
2024-07-18 13:30:00 +0100

    Remove redundant code

--- src/textual/widgets/_input.py
+++ src/textual/widgets/_input.py
@@ -394,9 +394,6 @@ class Input(Widget, can_focus=True):
                 self._cursor_visible = True
                 self._blink_timer.pause()
 
-    def watch__cursor_visible(self, cursor_visible: bool) -> None:
-        self.refresh()
-
     @property
     def cursor_screen_offset(self) -> Offset:
         """The offset of the cursor of this input in screen-space. (x, y)/(column, row)"""
@@ -508,7 +505,6 @@ class Input(Widget, can_focus=True):
     def _toggle_cursor(self) -> None:
         """Toggle visibility of cursor."""
         self._cursor_visible = not self._cursor_visible
-        self.refresh()
 
     def _on_mount(self, _: Mount) -> None:
         self._blink_timer = self.set_interval(
@@ -547,7 +543,6 @@ class Input(Widget, can_focus=True):
         event.stop()
 
     async def _on_click(self, event: events.Click) -> None:
-        print("click!")
         offset = event.get_content_offset(self)
         if offset is None:
             return
@@ -565,13 +560,11 @@ class Input(Widget, can_focus=True):
             self.cursor_position = len(self.value)
 
     async def _on_mouse_down(self, event: events.MouseDown) -> None:
-        print("mouse down!")
         self._cursor_visible = True
         if self.cursor_blink and self._blink_timer:
             self._blink_timer.pause()
 
     async def _on_mouse_up(self, event: events.MouseUp) -> None:
-        print("mouse up!")
         if self.cursor_blink and self._blink_timer:
             self._blink_timer.reset()
 
