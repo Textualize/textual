d02ed0134cdf485fd81ec60d49b11dc5c1bf0a59
Will McGugan <willmcgugan@gmail.com>
2024-07-23 12:06:06 +0100

    pre await callback

--- src/textual/await_complete.py
+++ src/textual/await_complete.py
@@ -1,25 +1,44 @@
 from __future__ import annotations
 
 from asyncio import Future, gather
-from typing import Any, Awaitable, Generator
+from typing import TYPE_CHECKING, Any, Awaitable, Generator
 
 import rich.repr
 from typing_extensions import Self
 
 from .message_pump import MessagePump
 
+if TYPE_CHECKING:
+    from .types import CallbackType
+
 
 @rich.repr.auto(angular=True)
 class AwaitComplete:
     """An 'optionally-awaitable' object which runs one or more coroutines (or other awaitables) concurrently."""
 
-    def __init__(self, *awaitables: Awaitable) -> None:
+    def __init__(
+        self, *awaitables: Awaitable, pre_await: CallbackType | None = None
+    ) -> None:
         """Create an AwaitComplete.
 
         Args:
             awaitables: One or more awaitables to run concurrently.
         """
+        self._awaitables = awaitables
         self._future: Future[Any] = gather(*awaitables)
+        self._pre_await: CallbackType | None = pre_await
+
+    def __rich_repr__(self) -> rich.repr.Result:
+        yield self._awaitables
+        yield "pre_await", self._pre_await, None
+
+    def set_pre_await_callback(self, pre_await: CallbackType | None) -> None:
+        """Set a callback to run prior to awaiting.
+
+        Args:
+            pre_await: A callback.
+        """
+        self._pre_await = pre_await
 
     def call_next(self, node: MessagePump) -> Self:
         """Await after the next message.
@@ -34,6 +53,9 @@ class AwaitComplete:
         return await self
 
     def __await__(self) -> Generator[Any, None, Any]:
+        _rich_traceback_omit = True
+        if self._pre_await is not None:
+            self._pre_await()
         return self._future.__await__()
 
     @property
