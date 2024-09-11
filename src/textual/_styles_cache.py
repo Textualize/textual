3833133d898d55d2d6b8579300f4e7dc65ac5b74
Darren Burns <darrenb900@gmail.com>
2024-09-11 11:32:01 +0100

    Remove print for debugging from styles_cache

--- src/textual/_styles_cache.py
+++ src/textual/_styles_cache.py
@@ -315,7 +315,6 @@ class StylesCache:
             """Apply effects to segments inside the border."""
             if styles.has_rule("hatch") and styles.hatch != "none":
                 character, color = styles.hatch
-                print(character, color)
                 if character != " " and color.a > 0:
                     hatch_style = Style.from_color(
                         (background + color).rich_color, background.rich_color
