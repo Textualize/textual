f3b545fb0d2e75a221cd07cdaac47c42b9e374b5
Will McGugan <willmcgugan@gmail.com>
2024-09-23 15:08:37 +0100

    disable kitty protocol

--- src/textual/drivers/linux_inline_driver.py
+++ src/textual/drivers/linux_inline_driver.py
@@ -310,6 +310,7 @@ class LinuxInlineDriver(Driver):
         """Stop application mode, restore state."""
         self._disable_bracketed_paste()
         self.disable_input()
+        self.write("\x1b[<u")  # Disable kitty protocol
         self.write("\x1b[J")
 
         if self.attrs_before is not None:
