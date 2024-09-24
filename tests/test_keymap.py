2562c86de916122652bb5feed1471786e1d02c14
Darren Burns <darrenb900@gmail.com>
2024-09-24 11:48:07 +0100

    Testing parent and child bindings overridden by keymap in case of different ids

--- tests/test_keymap.py
+++ tests/test_keymap.py
@@ -72,6 +72,11 @@ async def test_keymap_with_unknown_id_is_noop():
 
 
 async def test_keymap_inherited_bindings_same_id():
+    """When a child widget inherits from a parent widget, if they have
+    a binding with the same ID, then both parent and child bindings will
+    be overridden by the keymap (assuming the keymap has a mapping with the
+    same ID)."""
+
     parent_counter = 0
     child_counter = 0
 
@@ -122,6 +127,70 @@ async def test_keymap_inherited_bindings_same_id():
         assert child_counter == 0
 
         # New binding is active, child is focused - action called.
+        await pilot.press("i")
+        assert parent_counter == 1
+        assert child_counter == 1
+
+
+async def test_keymap_child_with_different_id_overridden():
+    """Ensures that overriding a parent binding doesn't influence a child
+    binding with a different ID."""
+
+    parent_counter = 0
+    child_counter = 0
+
+    class Parent(Widget, can_focus=True):
+        BINDINGS = [
+            Binding(key="x", action="increment", id="parent.increment"),
+        ]
+
+        def action_increment(self) -> None:
+            nonlocal parent_counter
+            parent_counter += 1
+
+    class Child(Parent):
+        BINDINGS = [
+            Binding(key="x", action="increment", id="child.increment"),
+        ]
+
+        def action_increment(self) -> None:
+            nonlocal child_counter
+            child_counter += 1
+
+    class MyApp(App[None]):
+        def compose(self) -> ComposeResult:
+            yield Parent()
+            yield Child()
+
+        def get_keymap(self) -> Keymap:
+            return Keymap(
+                {
+                    "parent.increment": "i",
+                }
+            )
+
+    app = MyApp()
+    async with app.run_test() as pilot:
+        # Default binding is unbound due to keymap.
+        await pilot.press("x")
+        assert parent_counter == 0
+        assert child_counter == 0
+
+        # New binding is active, parent is focused - action called.
+        await pilot.press("i")
+        assert parent_counter == 1
+        assert child_counter == 0
+
+        # Tab to focus the child.
+        await pilot.press("tab")
+
+        # Default binding is still active on the child.
+        await pilot.press("x")
+        assert parent_counter == 1
+        assert child_counter == 1
+
+        # The binding from the keymap only affects the parent, so
+        # pressing it with the child focused does nothing.
         await pilot.press("i")
         assert parent_counter == 1
         assert child_counter == 1
