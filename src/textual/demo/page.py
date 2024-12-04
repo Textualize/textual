eef9e493d56e292c0a68d148dca9e3fbde8266e4
Will McGugan <willmcgugan@gmail.com>
2024-12-04 10:35:01 +0000

    change copy

--- src/textual/demo/page.py
+++ src/textual/demo/page.py
@@ -59,7 +59,7 @@ class PageScreen(Screen):
         Binding(
             "c",
             "show_code",
-            "Show code",
+            "Code",
             tooltip="Show the code used to generate this screen",
         )
     ]
