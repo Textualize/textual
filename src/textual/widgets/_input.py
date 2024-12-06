8a09562b12d85d3bb7358354608a1be8e13498d8
Will McGugan <willmcgugan@gmail.com>
2024-12-06 17:05:11 +0000

    no need to sort

--- src/textual/widgets/_input.py
+++ src/textual/widgets/_input.py
@@ -1009,5 +1009,5 @@ class Input(ScrollView):
     def action_paste(self) -> None:
         """Paste from the local clipboard."""
         clipboard = self.app._clipboard
-        start, end = sorted(self.selection)
+        start, end = self.selection
         self.replace(clipboard, start, end)
