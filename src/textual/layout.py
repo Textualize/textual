c1f46209d8dcfbeff6a47b84c92651f9e75bead7
Will McGugan <willmcgugan@gmail.com>
2025-01-08 18:30:43 +0000

    changelog

--- src/textual/layout.py
+++ src/textual/layout.py
@@ -260,6 +260,7 @@ class Layout(ABC):
             if not widget.styles.is_docked and all(
                 child.styles.is_dynamic_height for child in widget.displayed_children
             ):
+                # An exception for containers with all dynamic height widgets
                 arrangement = widget._arrange(
                     Size(width, container.height - widget.gutter.height)
                 )
