f426c16366f79825223f8163c14a3a6ab403e3d7
Plywood9 <194300559+Plywood9@users.noreply.github.com>
2025-01-08 22:06:35 +0000

    fix busy loop on lagging timer

--- src/textual/timer.py
+++ src/textual/timer.py
@@ -153,7 +153,7 @@ class Timer:
             next_timer = start + ((count + 1) * _interval)
             now = _time.get_time()
             if self._skip and next_timer < now:
-                count += 1
+                count = int((now - start) / _interval + 1)
                 continue
             now = _time.get_time()
             wait_time = max(0, next_timer - now)
