6e4f77bb48080aacac03c6141eeed0cf11b5e92e
Will McGugan <willmcgugan@gmail.com>
2025-06-24 08:45:25 +0100

    sponsorship message

--- src/textual/__main__.py
+++ src/textual/__main__.py
@@ -1,5 +1,18 @@
+from rich import print
+from rich.panel import Panel
+
 from textual.demo.demo_app import DemoApp
 
 if __name__ == "__main__":
     app = DemoApp()
     app.run()
+    print(
+        Panel.fit(
+            "[b magenta]Hope you liked the demo![/]\n\n"
+            "Please consider sponsoring me if you get value from my work.\n\n"
+            "Even the price of a ☕ can brighten my day!\n\n"
+            "https://github.com/sponsors/willmcgugan",
+            border_style="red",
+            title="Consider sponsoring",
+        )
+    )
