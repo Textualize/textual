79b5f4c7bf03bc04d500756959e5cdf0e2ad2ac5
Akshat Baranwal <akshatvaidik@gmail.com>
2025-09-10 04:04:06 +0530

    Add snapshot test for header alignment

--- tests/snapshot_tests/snapshot_apps/header_alignment.py
+++ tests/snapshot_tests/snapshot_apps/header_alignment.py
@@ -1,11 +1,11 @@
-from textual.app import App, ComposeResult  # Add ComposeResult
+from textual.app import App, ComposeResult  
 from textual.widgets import Header
 
 class HeaderAlignmentApp(App):
     TITLE = "Title"
     SUB_TITLE = "Subtitle"
 
-    def compose(self) -> ComposeResult:  # Add return type
+    def compose(self) -> ComposeResult:  
         yield Header()
 
 if __name__ == "__main__":
