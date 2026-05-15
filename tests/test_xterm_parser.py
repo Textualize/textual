603c3d73d0272baa5e419595b27f13a7c6e6dbba
Will McGugan <willmcgugan@gmail.com>
2026-05-15 17:54:00 +0700

    Tests

--- tests/test_xterm_parser.py
+++ tests/test_xterm_parser.py
@@ -191,6 +191,8 @@ def test_double_escape(parser):
         ("\x1b[65;4u", "alt+shift+a"),
         ("\x1bA", "alt+shift+a"),
         ("\x1b[120;7u", "alt+ctrl+x"),
+        ("\x1b[57443;3u", "left_alt"),
+        ("\x1b[127;3u", "alt+backspace"),
     ],
 )
 def test_keys(parser, sequence: str, key: str) -> None:
